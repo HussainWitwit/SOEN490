2dd1a4f23c2db35029a764b9f5d080e12d5e8443
DariushN <35940376+DariushN@users.noreply.github.com>
2020-10-15 01:49:15 -0400

    RE-76 - Add factory design pattern for recommendation jobs

---
+++ RecommendationEngine/RecommendationEngine/Models/Application/RecommendationJob/RecommendationJobFactory.cs
@@ -0,0 +1,21 @@
+﻿using RecommendationEngine.Models.Application.Recommendation;
+
+namespace RecommendationEngine.Models.Application.RecommendationJob
+{
+    public class RecommendationJobFactory
+    {
+
+        public IRecommendationJob createRecommendationJob(RecommendationSchedule schedule)
+        {
+            switch (schedule.Type)
+            {
+                case "Yearly Wash Optimization": 
+                    return new YearlyWashOptimizationRecommendationJob();
+                case "Wind Turbine Change":
+                    return new WindTurbineChangeRecommendationJob();
+                default:
+                    throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ RecommendationEngine/RecommendationEngine/Models/Application/RecommendationJob/WindTurbineChangeRecommendationJob.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Quartz;
+
+namespace RecommendationEngine.Models.Application.RecommendationJob
+{
+    public class WindTurbineChangeRecommendationJob: IRecommendationJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
