9c72abc97bcfce758cc2a3a3c2ac1d6da7fcbfc7
Badreddine Loulidi <badred123456@gmail.com>
2020-10-27 01:00:02 -0400

    RE-126 - Added a notification bell with mocked data

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/NavMenu.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/NavMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import NotifyMe from 'react-notification-timeline';
 import './NavMenu.css';
 
 const Navigation = styled.nav`
@@ -12,32 +13,56 @@ const breadcrumb = {
 }
 
 function NavMenu(props) {
+  const [data, setData] = React.useState(
+    [
+      {
+        "update": "70 new employees are shifted",
+        "timestamp": 1596119688264
+      },
+      {
+        "update": "Time to take a Break, TADA!!!",
+        "timestamp": 1596119686811
+      }
+    ])
 
   function isLast(index) {
     return index === props.crumbs.length - 1;
   }
 
   return (
-      <Navigation>
-        <ol className="breadcrumb" style={breadcrumb}>
-          {
-            props.crumbs.map((crumb, ci) => {
-              const disabled = isLast(ci) ? 'disabled' : '';
+    <Navigation>
+      <ol className="breadcrumb" style={breadcrumb}>
+        {
+          props.crumbs.map((crumb, ci) => {
+            const disabled = isLast(ci) ? 'disabled' : '';
 
-              return (
-                <li
-                  key={ci}
-                  className="breadcrumb-item align-items-center"
-                >
-                  <button className={`btn btn-sm shadow-none ${disabled}`} onClick={() => props.selected(crumb)}>
-                    {crumb}
-                  </button>
-                </li>
-              );
-            })
-          }
-        </ol>
-      </Navigation>
+            return (
+              <li
+                key={ci}
+                className="breadcrumb-item align-items-center"
+              >
+                <button className={`btn btn-sm shadow-none ${disabled}`} onClick={() => props.selected(crumb)}>
+                  {crumb}
+                </button>
+              </li>
+            );
+          })
+        }
+        <div class="notification_bell">
+          <NotifyMe
+            data={data}
+            storageKey='notific_key'
+            notific_key='timestamp'
+            notific_value='update'
+            heading='Notification Alerts'
+            sortedByKey={false}
+            showDate={true}
+            size={20}
+            color="black"
+          />
+        </div>
+      </ol>
+    </Navigation>
   );
 }
 
