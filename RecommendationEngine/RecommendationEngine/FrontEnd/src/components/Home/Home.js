51f555a76a7e35eaac39cc0fad381178fb5caadb
Christophe S. Bahenduzi <46238950+csbduzi@users.noreply.github.com>
2020-10-28 20:09:11 -0400

    RE - 9 - Refactoring of the files/folders + Adding the buttons, search bar, etc

---
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ManageRecommendationTable from "../Table Component/ManageRecommendationTable"
+
+function Home (props){
+  return (
+    <div id="background">
+    <ManageRecommendationTable/>
+    </div>
+  );
+}
+
+export default Home;
