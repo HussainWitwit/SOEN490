04346ca1b73830f883ec03b1c4d92acca17fa95f
Alain Job Uzarama Uwe <alainjobu@hotmail.com>
2020-10-30 01:15:54 -0400

    Added FIXME: and TODO:

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/Layout.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/Layout.js
@@ -3,6 +3,8 @@ import { Container } from 'reactstrap';
 import NavMenu from './NavMenu/NavMenu';
 
 function Layout (props) {
+  //FIXME: This hook shouldn't be here
+  //TODO: The left Main Menu should be declared at this level. and the top-bar should be a sub-component of the mainMenu.
   const [crumbs, setCrumbs] = React.useState(['All Portfolio', '23-kahuku', '001-kahuku']);
 
   const selected = (crumb) => {
