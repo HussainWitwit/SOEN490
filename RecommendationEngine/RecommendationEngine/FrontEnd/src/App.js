ade8936e73b802106710d292082f0fb0ac8feeed
Kenza <kenza.boulisfane@gmail.com>
2020-10-31 17:22:15 -0400

    RE-16 - Implement SideMenu component
    - This is just an initial draft to get a feel of the Drawer component
    - Missing icons
    - Missing nested list
    
    Co-Authored-By: HussainWitwit <40703467+HussainWitwit@users.noreply.github.com>

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/App.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {Route} from 'react-router';
+import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import {FetchData} from './components/FetchData';
+import { FetchData } from './components/FetchData';
 import Counter from './components/Counter';
+import SideMenu from './components/SideMenu';
+
+
 
 import './custom.css'
 
 function App (props) {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
+  return (
+    <Layout>
+      <Route exact path='/' component={Home} />
+      <Route path='/counter' component={Counter} />
+      <Route path='/fetch-data' component={FetchData} />
+      <Route path='/side-menu' component={SideMenu} />
+    </Layout>
+  );
 }
 
 export default App;
