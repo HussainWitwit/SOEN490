f43019574e0604772b551915f12e3c25ba1f4398
DariushN <35940376+DariushN@users.noreply.github.com>
2020-11-01 03:27:52 -0500

    RE-125 - Refactor project structure to add dependency injection for repositories

---
+++ RecommendationEngine/RecommendationEngine/Repositories/RecommendationRunRepository.cs
@@ -0,0 +1,16 @@
+﻿using Interfaces.Repositories;
+using Models.DB;
+using RecommendationEngine.Services.ExternalAPI;
+
+namespace RecommendationEngine.Repositories
+{
+    public class RecommendationRunRepository: IRecommendationRunRepository
+    {
+        private RecommendationEngineDBContext _recommendationEngineDb;
+
+        public RecommendationRunRepository(RecommendationEngineDBContext recommendationEngineDb)
+        {
+            _recommendationEngineDb = recommendationEngineDb;
+        }
+    }
+}
---
+++ RecommendationEngine/RecommendationEngine/Repositories/RecommendationSchedulerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Interfaces.Repositories;
+using Models.DB;
+using RecommendationEngine.Services.ExternalAPI;
+
+namespace RecommendationEngine.Repositories
+{
+    public class RecommendationSchedulerRepository: IRecommendationSchedulerRepository
+    {
+        private RecommendationEngineDBContext _recommendationEngineDb;
+
+        public RecommendationSchedulerRepository(RecommendationEngineDBContext recommendationEngineDb)
+        {
+            _recommendationEngineDb = recommendationEngineDb;
+        }
+    }
+}
