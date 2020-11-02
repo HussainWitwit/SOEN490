68220f683fb5dcc7f96460db823ac75d33629297
DariushN <35940376+DariushN@users.noreply.github.com>
2020-11-02 18:41:54 -0500

    RE-5 - Update db models

--- RecommendationEngine/Models/DB/DBRecommendationType.cs
+++ RecommendationEngine/Models/DB/DBRecommendationType.cs
@@ -11,6 +11,8 @@ namespace Models.DB
         public int RecommendationTypeId { get; set; }
         public string Type { get; set; }
         public string DisplayText { get; set; }
+        public string Description { get; set; }
+        public string EnergyType { get; set; }
         public virtual ICollection<DBRecommendationSchedule> SchedulesOfTypeList { get; set; }
         public virtual ICollection<DBRecommendationParameter> DefaultParametersList { get; set; }
     }
