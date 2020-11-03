55aa092b96ea86b778b7aa375435db36d5ce8e72
DariushN <35940376+DariushN@users.noreply.github.com>
2020-11-03 18:59:46 -0500

    RE-116 - Inject logger to template recommendation jobs

--- RecommendationEngine/RecommendationScheduler/RecommendationTypes/TurbineChangeRecommendation.cs
+++ RecommendationEngine/RecommendationScheduler/RecommendationTypes/TurbineChangeRecommendation.cs
@@ -1,8 +1,10 @@
-﻿namespace RecommendationEngine.RecommendationScheduler.RecommendationTypes
+﻿using Models.DB;
+
+namespace RecommendationScheduler.RecommendationTypes
 {
     public class TurbineChangeRecommendation: IRecommendationType
     {
-        public void ExecuteAlgorithm()
+        public void ExecuteAlgorithm(DBRecommendationJob job)
         {
             throw new System.NotImplementedException();
         }
