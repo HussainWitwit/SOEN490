9fa28db7b591d232140c661897716534484fa021
Badreddine Loulidi <badred123456@gmail.com>
2020-11-03 20:53:49 -0500

    RE-126 - Removed eslint precomit formating and removed unused components

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/App.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import { FetchData } from './components/FetchData';
-import Counter from './components/Counter';
-
 
 
 import './custom.css'
@@ -13,8 +10,6 @@ function App (props) {
   return (
     <Layout>
       <Route exact path='/' component={Home} />
-      <Route path='/counter' component={Counter} />
-      <Route path='/fetch-data' component={FetchData} />
     </Layout>
   );
 }
