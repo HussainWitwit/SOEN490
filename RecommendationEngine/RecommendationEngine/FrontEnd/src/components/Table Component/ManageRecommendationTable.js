88af9420a723e033eb085f4c6f9814e9f194c545
CSB <cbahenduzi@gmail.com>
2020-11-06 07:09:23 -0500

    RE-5-First Part of  the Add Recommendation FE Template

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/Table Component/ManageRecommendationTable.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/Table Component/ManageRecommendationTable.js
@@ -17,8 +17,20 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton'
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import './ManageRecommendationTable.css';
+import { Typography } from '@material-ui/core';
 
 export const CssTextField = withStyles({
 
@@ -229,6 +241,16 @@ function ManageRecommendationTable() {
     const [orderBy, setOrderBy] = React.useState('');
     const [page, setPage] = React.useState(0);
     const [dense, setDense] = React.useState(false);
+    const [openFirst, setOpenFirst] = React.useState(false);
+    const [openSecond, setOpenSecond] = React.useState(false);
+    const [openThird, setOpenThird] = React.useState(false);
+    const [openFourth, setOpenFourth] = React.useState(false)
+    const [back, setBack] = React.useState(false);
+    const [state, setState] = React.useState({
+        age: '',
+        name: 'hai',
+    });
+
     //TODO:Set the hooks for the Title, Subtitle, Button Boolean, 
     //TODO:Set hook array for the name of the columns of the table and the filter list 
 
@@ -278,6 +300,98 @@ function ManageRecommendationTable() {
     // handling the empty rows for the method that compresses the table rows
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, mockRows.length - page * rowsPerPage);
 
+    const handleClickOpenFirst = () => {
+        setOpenFirst(true);
+    }
+
+    const handleCloseFirst = () => {
+        setOpenFirst(false);
+    }
+
+    const handleClickOpenSecond = () => {
+        setOpenSecond(true);
+        setOpenFirst(false)
+    }
+    const handleClickOpenThird = () => {
+        setOpenThird(true);
+        setOpenSecond(false);
+    }
+
+    const handleClickOpenFourth = () => {
+        setOpenFourth(true);
+        setOpenThird(false);
+    }
+
+    const handleDone = () => {
+        setOpenFirst(false);
+        setOpenSecond(false);
+        setOpenThird(false);
+        setOpenFourth(false);
+    }
+
+    const handleClose = () => {
+        setOpenFirst(false);
+        setOpenSecond(false);
+        setOpenThird(false);
+        setOpenFourth(false);
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        setState({
+            ...state,
+            [name]: event.target.value,
+        });
+    };
+
+    function FormRow() {
+        return (
+            <React.Fragment>
+                <Grid item xs={4}>
+                    <button className="gridBtn">
+                        <Paper id="paperTheme">
+                            <Typography id="gridBoxTitle">
+                                Wash Optmization
+                        </Typography></Paper>
+                    </button>
+                    <button className="gridBtn">
+                        <Paper id="paperTheme">
+                            <Typography id="gridBoxTitle">
+                                Fuse Replacement
+                        </Typography></Paper>
+                    </button>
+                    <button className="gridBtn">
+                        <Paper id="paperTheme">
+                            <Typography id="gridBoxTitle">
+                                ETC
+                        </Typography>
+                        </Paper>
+                    </button>
+                </Grid>
+                <Grid item xs={4}>
+                    <button className="gridBtn">
+                        <Paper id="paperTheme">
+                            <Typography id="gridBoxTitle">
+                                Gearbox Replacement
+                        </Typography></Paper>
+                    </button>
+                    <button className="gridBtn">
+                        <Paper id="paperTheme">
+                            <Typography id="gridBoxTitle">
+                                Panel Angle
+                        </Typography></Paper>
+                    </button>
+                    <button className="gridBtn">
+                        <Paper id="paperTheme">
+                            <Typography id="gridBoxTitle">
+                                Other
+                        </Typography></Paper>
+                    </button>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
     return (
         <div id="main-container">
             <div>
@@ -290,7 +404,147 @@ function ManageRecommendationTable() {
                         <h6 id="subtitle">Add, edit, delete and browse the configured recommendation</h6>
                     </Grid >
                     <Grid item>
-                        <Button id='recBtn'>Create Recommendation</Button>
+                        <Button id='recBtn' onClick={handleClickOpenFirst}>Create Recommendation</Button>
+
+                        <Dialog open={openFirst} onClose={handleClose} aria-labelledby="form-dialog-title">
+                            <IconButton aria-label="close" id="closeButton" onClick={handleClose}>
+                                <CloseIcon />
+                            </IconButton>
+                            <DialogTitle id="form-dialog-title" className="dialogTitle">Template Configuration</DialogTitle>
+                            <DialogContent className="dialogPaper">
+                                <div id="gridRoot">
+                                    <Grid container spacing={3}>
+                                        <Grid item sm container item xs={10} spacing={3}>
+                                            <FormRow />
+                                        </Grid>
+                                    </Grid>
+                                </div>
+
+                            </DialogContent>
+                            <DialogActions>
+                                <Button id="cancelBtn" onClick={handleCloseFirst}>Cancel</Button>
+                                <Button id="nextBtn" onClick={handleClickOpenSecond}>Next</Button>
+                            </DialogActions>
+                        </Dialog>
+
+                        <Dialog open={openSecond} onClose={handleClose} aria-labelledby="form-dialog-title">
+                            <IconButton aria-label="close" id="closeButton" onClick={handleClose}>
+                                <CloseIcon />
+                            </IconButton>
+                            <DialogTitle id="form-dialog-title">Wash Optmization Configuration</DialogTitle>
+                            <DialogContent className="dialogPaper">
+                                <DialogContentText>Hello My Friend!</DialogContentText>
+                                <CssTextField autoFocus margin="dense" id="name" type="text" placeholder="Wash Optmization P20" ></CssTextField>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button id="cancelBtn" onClick={handleClickOpenFirst}>Back</Button>
+                                <Button id="nextBtn" onClick={handleClickOpenThird}>Next</Button>
+                            </DialogActions>
+                        </Dialog>
+
+                        <Dialog open={openThird} onClose={handleClose} aria-labelledby="form-dialog-title">
+                            <IconButton aria-label="close" id="closeButton" onClick={handleClose}>
+                                <CloseIcon />
+                            </IconButton>
+                            <DialogTitle id="form-dialog-title">Addtional Configuration</DialogTitle>
+                            <DialogContent className="dialogPaper">
+
+                                <DialogContentText>Name </DialogContentText>
+                                <FormControl id="formControl">
+                                    <NativeSelect
+                                        value={state.age}
+                                        onChange={handleChange}
+                                        name="age"
+                                        inputProps={{ 'aria-label': 'age' }}
+                                    >
+                                        <option value="">None</option>
+                                        <option value={10}>Ten</option>
+                                        <option value={20}>Twenty</option>
+                                        <option value={30}>Thirty</option>
+                                    </NativeSelect>
+                                </FormControl>
+                                <DialogContentText>Asset </DialogContentText>
+                                <FormControl id="formControl">
+                                    <NativeSelect
+                                        value={state.age}
+                                        onChange={handleChange}
+                                        name="age"
+                                        inputProps={{ 'aria-label': 'age' }}
+                                    >
+                                        <option value="">None</option>
+                                        <option value={10}>Ten</option>
+                                        <option value={20}>Twenty</option>
+                                        <option value={30}>Thirty</option>
+                                    </NativeSelect>
+                                </FormControl>
+
+
+                                <DialogContentText>Granularity </DialogContentText>
+                                <FormControl id="formControl">
+                                    <NativeSelect
+                                        value={state.age}
+                                        onChange={handleChange}
+                                        name="age"
+                                        inputProps={{ 'aria-label': 'age' }}
+                                    >
+                                        <option value="">None</option>
+                                        <option value={10}>Ten</option>
+                                        <option value={20}>Twenty</option>
+                                        <option value={30}>Thirty</option>
+                                    </NativeSelect>
+                                </FormControl>
+
+
+                                <DialogContentText>Parameter </DialogContentText>
+                                <FormControl id="formControl">
+                                    <NativeSelect
+                                        value={state.age}
+                                        onChange={handleChange}
+                                        name="age"
+                                        inputProps={{ 'aria-label': 'age' }}
+                                    >
+                                        <option value="">None</option>
+                                        <option value={10}>Ten</option>
+                                        <option value={20}>Twenty</option>
+                                        <option value={30}>Thirty</option>
+                                    </NativeSelect>
+                                </FormControl>
+
+
+                            </DialogContent>
+                            <DialogActions>
+                                <Button id="cancelBtn" onClick={handleClickOpenSecond}>Back</Button>
+                                <Button id="nextBtn" onClick={handleClickOpenFourth}>Next</Button>
+                            </DialogActions>
+                        </Dialog>
+
+                        <Dialog open={openFourth} onClose={handleClose} aria-labelledby="form-dialog-title">
+                            <IconButton aria-label="close" id="closeButton" onClick={handleClose}>
+                                <CloseIcon />
+                            </IconButton>
+                            <DialogTitle id="form-dialog-title">Configuration Confirmation</DialogTitle>
+                            <DialogContent id="confirmationDialog">
+                                <DialogContentText>Name:</DialogContentText>
+                                <DialogContentText>Asset:</DialogContentText>
+                                <DialogContentText>Asset Type:</DialogContentText>
+                                <DialogContentText>Granularity:</DialogContentText>
+                                <DialogContentText>Frequency of Repition:</DialogContentText>
+                                <DialogContentText>Periodicity:</DialogContentText>
+                                <DialogContentText>Day of The Week:</DialogContentText>
+                                <DialogContentText>Start Date:</DialogContentText>
+                                <DialogContentText>End Date:</DialogContentText>
+                                <DialogContentText>Hour Repeated:</DialogContentText>
+                                <DialogContentText>Created On:</DialogContentText>
+                                <DialogContentText>Created By:</DialogContentText>
+                                <DialogContentText>Occurencies Data Time:</DialogContentText>
+                                <DialogContentText>Span:</DialogContentText>
+                                <DialogContentText>Parameter Value:</DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button id="cancelBtn" onClick={handleClickOpenSecond}>Back</Button>
+                                <Button id="nextBtn" onClick={handleDone}>Done</Button>
+                            </DialogActions>
+                        </Dialog>
                     </Grid>
                 </Grid>
                 <br></br>
@@ -380,7 +634,7 @@ function ManageRecommendationTable() {
                                                 key={row.title}
                                                 className="custom"
                                             >
-                                                <TableCell  className="custom">
+                                                <TableCell className="custom">
                                                 </TableCell>
                                                 <TableCell
                                                     component="th"
