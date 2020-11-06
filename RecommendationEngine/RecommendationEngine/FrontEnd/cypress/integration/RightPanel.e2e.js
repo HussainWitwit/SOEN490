f32427402a14bb7a8a95368bd7aec50934842da8
Kenza <kenza.boulisfane@gmail.com>
2020-11-06 14:25:57 -0500

    RE-66 - Add RightPanel system test

---
+++ RecommendationEngine/RecommendationEngine/FrontEnd/cypress/integration/RightPanel.e2e.js
@@ -0,0 +1,42 @@
+
+describe("RightPanel component", () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it("Finds all elements", () => {
+        cy.get('.breadcrumb > :nth-child(3) > .MuiButtonBase-root').click()
+        cy.wait(500)
+        cy.get('.Drawer-Header-Container').should('exist')
+        cy.wait(500)
+        cy.get('.Drawer-Header-Container > p').should('exist')
+        cy.wait(500)
+        cy.get('.material-icons').should('exist')
+        cy.wait(500)
+        cy.get('.Drawer-Header-Container > :nth-child(3) > .MuiIconButton-label > .MuiSvgIcon-root').should('exist')
+        cy.wait(500)
+        cy.get('.MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').should('exist')
+        cy.wait(500)
+        cy.get('.MuiAutocomplete-popupIndicator > .MuiIconButton-label > .MuiSvgIcon-root').should('exist')
+        cy.wait(500)
+        cy.get('.MuiTreeView-root').should('exist')
+        cy.wait(500)
+        cy.get(':nth-child(2) > .MuiDrawer-root > .MuiPaper-root').should('exist')
+        cy.wait(500)
+    })
+
+    it("Clicks  on all elements", () => {
+        cy.get('.breadcrumb > :nth-child(3) > .MuiButtonBase-root').click() // change button
+        cy.wait(500)
+        cy.get('.material-icons').click() // pin
+        cy.wait(500)
+        cy.get('.MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').type('Asset Title 1') // automplete
+        cy.wait(500)
+        cy.get('.MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').type('{downArrow}') // scroll
+        cy.wait(500)
+        cy.get('.material-icons').click() //pin
+        cy.wait(500)
+        cy.get(':nth-child(3) > .MuiIconButton-label > .MuiSvgIcon-root > path').click() //close
+        cy.wait(500)
+    })
+})
