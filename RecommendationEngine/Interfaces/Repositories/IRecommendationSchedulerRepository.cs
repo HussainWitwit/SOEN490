d163c4f52bcd85a6f005ea608c6604c6b7abf2d1
DariushN <35940376+DariushN@users.noreply.github.com>
2020-11-07 04:06:51 -0500

    RE-116 - Implement scheduler repository

--- RecommendationEngine/Interfaces/Repositories/IRecommendationSchedulerRepository.cs
+++ RecommendationEngine/Interfaces/Repositories/IRecommendationSchedulerRepository.cs
@@ -1,4 +1,6 @@
-﻿using Models.DB;
+﻿using System;
+using System.Collections.Generic;
+using Models.DB;
 
 namespace Interfaces.Repositories
 {
@@ -6,5 +8,9 @@ namespace Interfaces.Repositories
     {
         public DBRecommendationJob GetDbRecommendationJobById(int id);
         public DBRecommendationSchedule GetDbRecommendationScheduleById(int id);
+        public List<DBRecommendationSchedule> GetDbRecommendationSchedules();
+        public DBRecommendationJob AddRecommendationJob(DBRecommendationJob job);
+        public void UpdateRecommendationJobStatus(int jobId, string status);
+        public void UpdateRecommendationJobStatus(int jobId, string status, int jobDurationSeconds);
     }
 }
--- RecommendationEngine/RecommendationEngine/Repositories/RecommendationSchedulerRepository.cs
+++ RecommendationEngine/RecommendationEngine/Repositories/RecommendationSchedulerRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Models.DB;
@@ -22,8 +25,40 @@ namespace RecommendationEngine.Repositories
 
         public DBRecommendationSchedule GetDbRecommendationScheduleById(int id)
         {
-            return _recommendationEngineDb.RecommendationSchedules.Include(x=>x.RecommendationType).FirstOrDefault(x =>
+            return _recommendationEngineDb.RecommendationSchedules.Include(x=>x.RecommendationType).Include(x=>x.AssetsList).FirstOrDefault(x =>
                 x.RecommendationScheduleId == id);
+        }
+
+        public List<DBRecommendationSchedule> GetDbRecommendationSchedules()
+        {
+            return _recommendationEngineDb.RecommendationSchedules.Include(x => x.RecommendationType).ToList();
+        }
+
+        public DBRecommendationJob AddRecommendationJob(DBRecommendationJob job)
+        {
+            _recommendationEngineDb.RecommendationJobs.Add(job);
+            _recommendationEngineDb.SaveChanges();
+            return job;
+        }
+
+        public void UpdateRecommendationJobStatus(int jobId, string status)
+        {
+            DBRecommendationJob dbRecommendationJob = _recommendationEngineDb.RecommendationJobs.FirstOrDefault(x =>
+                x.RecommendationJobId == jobId);
+            if (dbRecommendationJob != null) dbRecommendationJob.Status = status;
+            _recommendationEngineDb.SaveChanges();
+        }
+
+        public void UpdateRecommendationJobStatus(int jobId, string status, int jobDurationSeconds)
+        {
+            DBRecommendationJob dbRecommendationJob = _recommendationEngineDb.RecommendationJobs.FirstOrDefault(x =>
+                x.RecommendationJobId == jobId);
+            if (dbRecommendationJob != null)
+            {
+                dbRecommendationJob.Status = status;
+                dbRecommendationJob.JobDuration = jobDurationSeconds;
+            }
+            _recommendationEngineDb.SaveChanges();
         }
     }
 }
