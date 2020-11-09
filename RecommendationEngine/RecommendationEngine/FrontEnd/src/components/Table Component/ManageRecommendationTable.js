bf9104288cd6c9a96abe35e42537fbb01dc75823
CSB <cbahenduzi@gmail.com>
2020-11-08 21:18:29 -0500

    RE-5-Commented out unnecessary methods for this sprint

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/Table Component/ManageRecommendationTable.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/Table Component/ManageRecommendationTable.js
@@ -95,15 +95,15 @@ const BootstrapInput = withStyles((theme) => ({
    * @param {*} orderBy
  
    */
-export function descendingComparator (a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
+// export function descendingComparator (a, b, orderBy) {
+//     if (b[orderBy] < a[orderBy]) {
+//         return -1;
+//     }
+//     if (b[orderBy] > a[orderBy]) {
+//         return 1;
+//     }
+//     return 0;
+// }
 
 // Same thing here
 
@@ -113,11 +113,11 @@ export function descendingComparator (a, b, orderBy) {
 * @param {*} orderBy 
 */
 
-export function getComparator (order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
+// export function getComparator (order, orderBy) {
+//     return order === 'desc'
+//         ? (a, b) => descendingComparator(a, b, orderBy)
+//         : (a, b) => -descendingComparator(a, b, orderBy);
+// }
 
 // Same for this function it takes in an array and a comparator
 
@@ -127,15 +127,15 @@ export function getComparator (order, orderBy) {
 * @param {} event 
 * @param {*} index 
 */
-export function stableSort (array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
+// export function stableSort (array, comparator) {
+//     const stabilizedThis = array.map((el, index) => [el, index]);
+//     stabilizedThis.sort((a, b) => {
+//         const order = comparator(a[0], b[0]);
+//         if (order !== 0) return order;
+//         return a[1] - b[1];
+//     });
+//     return stabilizedThis.map((el) => el[0]);
+// }
 
 const headCells = [
     { id: 'title', numeric: false, label: 'Title' },
@@ -982,9 +982,9 @@ function ManageRecommendationTable () {
                             />
 
                             <TableBody id="table-body">
-                                {stableSort(data, getComparator(order, orderBy))
-                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    .map((row, index) => {
+                                {/* {stableSort(data, getComparator(order, orderBy))*/
+                                    // slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                   data.map((row) => { 
                                         return (
                                             <TableRow
                                                 key={row.title}
@@ -1007,7 +1007,7 @@ function ManageRecommendationTable () {
                                                 <TableCell className="custom" id="tableBody">{row.createdOn}</TableCell>
                                             </TableRow>
                                         );
-                                    })}
+                                       })}  
                                 {emptyRows > 0 && (
                                     <TableRow className="center" classes={{ height: (dense ? 33 : 53) * emptyRows }}>
                                         <TableCell className="center" />
