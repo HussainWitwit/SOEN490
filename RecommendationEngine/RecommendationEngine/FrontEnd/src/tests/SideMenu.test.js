616faf516918503c28314d7d2735729103945bc0
Kenza <kenza.boulisfane@gmail.com>
2020-11-12 01:19:48 -0500

    RE-182 - Tweak SideMenu unit tests to refactoring
    - Added typescrit files to the compilable files by Jest
    - Change SideMenu tests

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/tests/SideMenu.test.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/tests/SideMenu.test.js
@@ -10,7 +10,6 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('SideMenu component', () => {
-    let component;
     const setState = jest.fn();
     const useStateSpy = jest.spyOn(React, 'useState');
     useStateSpy.mockImplementation((init) => [init, setState]);
@@ -35,28 +34,13 @@ describe('SideMenu component', () => {
         const output = shallow(<SideMenu />);
         let drawer = output.find(Drawer);
         expect(drawer).toHaveLength(1);
-        expect(output.find('#drawer')).toBeTruthy();
     });
 
     it('It finds the avatar container', () => {
         const output = shallow(<SideMenu />);
-        let drawer = output.find(Avatar);
-        expect(drawer).toHaveLength(1);
-        expect(output.find('#drawer')).toBeTruthy();
-    });
+        let avatar = output.find(Avatar);
+        expect(avatar).toHaveLength(1);
 
-    it('It opens nested items', () => {
-        const onButtonClickMock = jest.fn();
-        const wrapper = shallow(
-            <SideMenu handleClick={onButtonClickMock} />
-        );
-        wrapper.find('#recommendations')
-            .dive()
-            // .find('.main-titles')
-            // .simulate('click');
-            .invoke('onClick');
-
-        expect(onButtonClickMock).toHaveBeenCalledTimes(0); // step 3
     });
 
     it('Checks that Settings separator text is there', () => {
@@ -69,33 +53,6 @@ describe('SideMenu component', () => {
             let wrapper = shallow(<SideMenu />);
             let listItems = wrapper.find(ListItem);
             expect(listItems).toHaveLength(10);
-
-            listItems.filter('#dashboard').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#recommendations').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#manage').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#results').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#jobs').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#actions').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#work-orders').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#main-settings').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
-
-            listItems.filter('#notifications').simulate('click');
-            expect(wrapper.find(ListItem)).toHaveLength(10);
         });
     });
 });
