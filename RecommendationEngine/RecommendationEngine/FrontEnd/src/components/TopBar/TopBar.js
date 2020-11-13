e5c0b97144a5f93e9afeef5eda685828df30c754
Badreddine Loulidi <badred123456@gmail.com>
2020-11-13 00:11:50 -0500

    RE-205 - Refactored the breadcrumb component with the library material-ui

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/TopBar/TopBar.js
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/TopBar/TopBar.js
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
 import NotificationBell from '../Notification/NotificationBell';
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+import { GiWindTurbine } from "react-icons/gi";
+import { BiCube } from "react-icons/bi";
 import './TopBar.css';
 
-function TopBar (props) {
+function TopBar(props) {
 
   const [locationDetails, setLocationDetails] = React.useState(null);
   const [weatherDetails, setWeatherDetails] = React.useState(null);
+  const [isChangeClicked, setIsChangeClicked] = React.useState(false);
 
-  const getChangedAssetCrumbEvent = (value) => {
-    props.changeAsset(value);
-  }
+  const changeAssetEvent = () => {
+    props.changeAsset(!props.sharedChangeAssetValue || !isChangeClicked);
+    setIsChangeClicked(!props.sharedChangeAssetValue || !isChangeClicked);
+}
 
   // eslint-disable-next-line
   const getCurrentLocation = async () => {
@@ -35,7 +42,18 @@ function TopBar (props) {
   return (
     <div id="main-containter">
       <nav>
-        <Breadcrumb id="breadcrumb" changeAsset = {getChangedAssetCrumbEvent} sharedChangeAssetValue = {props.sharedChangeAssetValue}/>
+        <Breadcrumbs aria-label="breadcrumb" separator="›">
+          <Link color="inherit">
+          <BiCube />
+            All Portfolio
+          </Link>
+          <Link color="inherit">
+          <GiWindTurbine />
+            23-kahuku
+          </Link>
+          <Typography color="textPrimary">001-kahuku</Typography>
+        </Breadcrumbs>
+        <p className="change_button" onClick = {changeAssetEvent}>Change</p>
         <div id="weather-div" className="weather">
           <p>{weatherDetails ? parseInt(weatherDetails.temp) + "°C " + weatherDetails.description : "Waiting for data"}</p>
           <p>{locationDetails ? locationDetails.city : "Waiting for data"} {locationDetails ? " " + locationDetails.country_code : "Waiting for data"}</p>
