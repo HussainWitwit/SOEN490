cd52aca2c15a61203cebd24977c8f982f07273ca
DariushN <35940376+DariushN@users.noreply.github.com>
2020-11-20 22:09:04 -0500

    RE-202 - Refactor recommendation job to avoid duplication

--- RecommendationEngine/RecommendationScheduler/RecommendationJob/FuseReplacementRecommendationJob.cs
+++ RecommendationEngine/RecommendationScheduler/RecommendationJob/FuseReplacementRecommendationJob.cs
@@ -1,17 +1,8 @@
-﻿using System.Threading.Tasks;
-using Models.DB;
-using Quartz;
-
-namespace RecommendationScheduler.RecommendationJob
+﻿namespace RecommendationScheduler.RecommendationJob
 {
     public class FuseReplacementRecommendationJob: RecommendationJob
     {
-        public override Task Execute(IJobExecutionContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void CreateRecommendationJob()
+        protected override void ExecuteJob()
         {
             throw new System.NotImplementedException();
         }
