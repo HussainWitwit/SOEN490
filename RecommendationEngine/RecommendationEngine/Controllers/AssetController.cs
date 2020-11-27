6e493b46ce8c5d3ec3da6d39815767957034acb0
Zohal <zohalmir9@gmail.com>
2020-11-27 18:12:55 -0500

    merge conflict fix
    Co-authored-by: Loujain-Al-Nakeeb <loujain_alnakeeb@hotmail.com>

--- RecommendationEngine/RecommendationEngine/Controllers/AssetController.cs
+++ RecommendationEngine/RecommendationEngine/Controllers/AssetController.cs
@@ -1,7 +1,20 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
 using Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using Models.DB;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Quartz;
 using RecommendationEngine.ExceptionHandler;
+using RecommendationScheduler.RecommendationJob;
+using RecommendationScheduler.RecommendationTypes;
 
 namespace RecommendationEngine.Controllers
 {
@@ -10,10 +23,12 @@ namespace RecommendationEngine.Controllers
     public class AssetController : ControllerBase
     {
         private IAssetService _assetService;
+        private RecommendationEngineDBContext _dbContext;//
 
-        public AssetController(IAssetService assetService)
+        public AssetController(IAssetService assetService, RecommendationEngineDBContext dbContext)
         {
             _assetService = assetService;
+            _dbContext = dbContext;
         }
 
         [HttpGet("get")]
@@ -21,6 +36,43 @@ namespace RecommendationEngine.Controllers
         {
             try
             {
+                var parameters = _dbContext.RecommendationScheduleParameters.ToList();
+                var assetRecSchedule = new DBAssetRecommendationSchedule()
+                {
+                    Asset = _dbContext.Assets.Where(asset => asset.Name == "RENEW01_2070.92.003").FirstOrDefault(),
+                    AssetId = _dbContext.Assets.Where(asset => asset.Name == "RENEW01_2070.92.003").FirstOrDefault().AssetId,
+                };
+
+                var assetList = new List<DBAssetRecommendationSchedule>();
+                assetList.Add(assetRecSchedule);
+
+                _dbContext.AssetRecommendationSchedules.Add(assetRecSchedule);
+
+                var configuredRecommendation = new DBRecommendationSchedule
+                {
+                    ModifiedBy = "Voldemort",
+                    DisplayText = "Sample configured ywo",
+                    CreatedOn = new DateTime(2019, 01, 12),
+                    Description = "this is a description",
+                    Granularity = "yearly",
+                    Name = "SampleConfiguredRec",
+                    RecurrenceDatetime = new DateTime(2019, 05, 12),
+                    RecurrenceDayOfWeek = 2,
+                    PreferedScenario = "netSaving",
+                    RecommendationType = _dbContext.RecommendationTypes.Where(type => type.RecommendationTypeId == 1).FirstOrDefault(),
+                    ParametersList = parameters,
+                    AssetsList = assetList
+                };
+
+                _dbContext.RecommendationSchedules.Add(configuredRecommendation);
+
+                _dbContext.AssetRecommendationSchedules.FirstOrDefault().ScheduleId = _dbContext.RecommendationSchedules.Where(schedule => schedule.Name == "SampleConfiguredRec").FirstOrDefault().RecommendationScheduleId;
+                _dbContext.AssetRecommendationSchedules.FirstOrDefault().Schedule = _dbContext.RecommendationSchedules.Where(schedule => schedule.Name == "SampleConfiguredRec").FirstOrDefault();
+                _dbContext.SaveChanges();
+
+
+                DBRecommendationJob job = new DBRecommendationJob();
+                YearlyWashOptimizationRecommendation.ExecuteAlgorithm(job);
                 return Ok(_assetService.GetAssetsTreeview());
             }
             catch (GlobalException e)
