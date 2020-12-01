7465b866a1563bfe07e1ef99e05892c55507a871
CSB <cbahenduzi@gmail.com>
2020-12-01 04:59:09 -0500

    RE-5  - Refactoring of Dialog 1

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AddRecommendationDialog/Modals/TemplateConfigurationModal/ListTemplateItems.ts
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AddRecommendationDialog/Modals/TemplateConfigurationModal/ListTemplateItems.ts
@@ -1,67 +1,45 @@
 import { SvgIconTypeMap } from "@material-ui/core";
 import { OverridableComponent } from "@material-ui/core/OverridableComponent";
-import DashboardRoundedIcon from '@material-ui/icons/DashboardRounded';
-import ListAltRoundedIcon from '@material-ui/icons/ListAltRounded';
-import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded';
-import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
-import WorkOutlineRoundedIcon from '@material-ui/icons/WorkOutlineRounded';
-import BuildRoundedIcon from '@material-ui/icons/BuildRounded';
-import CheckCircleOutlineRoundedIcon from '@material-ui/icons/CheckCircleOutlineRounded';
-import TuneRoundedIcon from '@material-ui/icons/TuneRounded';
-import EventRoundedIcon from '@material-ui/icons/EventRounded';
 import { FaSolarPanel } from 'react-icons/fa';
-// import { GiSolarPower } from 'react-icons/fa';
-// import { GiGears } from 'react-icons/gi';
 import { BsFillGearFill } from 'react-icons/bs';
 import {GiBatteryPackAlt } from 'react-icons/gi';
-// import { FiBatteryCharging } from 'react-icons/fi';
-import {TiBatteryCharge } from 'react-icons/ti';
-// import { BsFillGearFill } from 'react-icons/fa';
 import { BiShapeTriangle } from 'react-icons/bi';
 import {  VscCircuitBoard } from 'react-icons/vsc';
 import { MdPermDeviceInformation } from 'react-icons/md';
-
 
 type ListTemplateObject = {
     name: string,
     listItemIcon: OverridableComponent<SvgIconTypeMap>,
 };
 
-type Separator = {
-    name: string;
-}
-
 var washOptimization: ListTemplateObject = {
     name: 'Wash Optimization',
     listItemIcon: FaSolarPanel,
-}
+};
 
 var gearboxReplacement: ListTemplateObject = {
     name: 'Gear Replacement',
     listItemIcon: BsFillGearFill,
-}
+};
 
 var panelAngle: ListTemplateObject = {
     name: 'Panel Angle',
     listItemIcon: BiShapeTriangle,
 };
 
-
 var fuseReplacement: ListTemplateObject = {
     name: 'Fuse Replacement',
     listItemIcon: GiBatteryPackAlt ,
-}
+};
 
 var etc: ListTemplateObject = {
     name: 'ETC',
     listItemIcon: VscCircuitBoard,
-}
+};
 
 var other: ListTemplateObject = {
     name: 'Other',
     listItemIcon: MdPermDeviceInformation,
-}
-
-
+};
 
 export const TemplateItems = [washOptimization, gearboxReplacement, fuseReplacement, panelAngle, etc, other]
--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AddRecommendationDialog/Modals/TemplateConfigurationModal/TemplateConfigurationModal.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AddRecommendationDialog/Modals/TemplateConfigurationModal/TemplateConfigurationModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import { useTransition, animated } from 'react-spring';
-import { Card, CardContent, Typography, CardMedia, Grid } from '@material-ui/core';
-import DialogContentText from '@material-ui/core/DialogContentText';
+import { animated } from 'react-spring';
+import { Typography } from '@material-ui/core';
 import './TemplateConfigurationModal.css'
 import { TemplateItems } from './ListTemplateItems.ts';
 import List from '@material-ui/core/List';
@@ -10,78 +9,108 @@ import Divider from '@material-ui/core/Divider';
 
 function TemplateConfigurationModal(props) {
 
-    const [itemPressed, setItemPressed] = useState(false);
+    const [itemPressed, setItemPressed] = useState("");
 
     const TemplateCard = (props) => {
-        return (        
-                <div id="item">
-                    <div>
-                        <props.icon id="templateIcon" />
-                    </div>
-                    <div>
-                        <Typography classes={{ root: 'title-label' }}>{props.name}</Typography>
-                    </div>
+        return (
+            <div id="item-template" onClick={props.onClick}>
+                <div>
+                    <props.icon id="template-icon" />
                 </div>
-      
-
+                <div>
+                    <Typography classes={{ root: 'template-label' }}>{props.name}</Typography>
+                </div>
+            </div>
         );
     }
+
     return (
         <animated.div id="modal-container" style={props.dialogStyle}>
             <div id="parent-div">
-                <div id="grid">
-                    {TemplateItems.map((item, index) => (<TemplateCard name={item.name} icon={item.listItemIcon} />))}
+                <div id="template-grid">
+                    {TemplateItems.map((item, index) => (
+                        <TemplateCard name={item.name}
+                            icon={item.listItemIcon}
+                            onClick={() => setItemPressed(item.name)}
+                        />))}
                 </div>
                 <div id="info-div">
-                    <List style={{maxHeight: '100%', overflow: 'auto'}}>
 
-                        <ListItem>
-                            <div className="onelinerAlign2">
-                                <DialogContentText id="templateLabel2">
-                                    <DialogContentText id="templateLabel">Description</DialogContentText>
-                                    <ul className="listAlign">
-                                        <li id="listItem">This recommendation is used to suggest the optimal time to wash your solar panels.</li>
-                                    </ul>
-                                </DialogContentText>
-                            </div>
-                        </ListItem>
-                        <Divider classes={{root: 'divider-item'}} />
-                        <ListItem>
-                            <div className="onelinerAlign2">
-                                <DialogContentText id="templateLabel2">
-                                    <DialogContentText id="templateLabel">Inputs</DialogContentText>
-                                    <ol className="listAlign2">
-                                        <li>Input 1</li>
-                                        <li>Input 2</li>
-                                        <li>Input 3</li>
-                                    </ol>
-                                </DialogContentText>
-                            </div>
-                        </ListItem>
-                        <Divider classes={{root: 'divider-item'}} />
-                        <ListItem>
-                            <div className="onelinerAlign2">
-                                <DialogContentText id="templateLabel2">
-                                    <DialogContentText id="templateLabel">Assets</DialogContentText>
-                                    <ul className="listAlign">
-                                        <li id="listItem">Solar Panels</li>
-                                    </ul>
-                                </DialogContentText>
-                            </div>
-                        </ListItem>
-                        <Divider classes={{root: 'divider-item'}} />
-                        <ListItem>
-                            <div className="onelinerAlign2">
-                                <DialogContentText id="templateLabel2">
-                                    <DialogContentText id="templateLabel">Algorithm</DialogContentText>
-                                    <ul className="listAlign">
-                                        <li id="listItem">WO Algorithm</li>
-                                    </ul>
-                                </DialogContentText>
-                            </div>
-                        </ListItem>
-                    </List>
 
+
+                    <div id="item-info1"> 
+                    <Typography classes={{ root: 'title-dialog-1' }}>Description {itemPressed == "" ? "" : "of " + itemPressed}</Typography>
+                    <Typography classes={{ root: 'subtitle-dialog-1' }}>This recommendation is used to suggest the optimal time to wash your solar panels.</Typography>
+                    </div>
+                    <Divider classes={{ root: 'divider-item' }} />
+                    <div id="item-info2">
+                    <Typography classes={{ root: 'title-dialog-1' }}>Inputs</Typography>
+                            <ol id="list-align">
+                                <Typography classes={{ root: 'list-dialog-1' }}>
+                                    <li>Input 1</li>
+                                    <li>Input 2</li>
+                                    <li>Input 3</li>
+                                </Typography>
+                            </ol>
+                            
+                    </div>
+                    <Divider classes={{ root: 'divider-item' }} />
+                    <div id="item-info3">
+                    <Typography classes={{ root: 'title-dialog-1' }}>Assets</Typography>
+                    <Typography classes={{ root: 'subtitle-dialog-1' }}>Solar Panels</Typography>
+                    </div>
+                    <Divider classes={{ root: 'divider-item' }} />
+                    <div id="item-info4">
+                    <Typography classes={{ root: 'title-dialog-1' }}>Algorithm</Typography>
+                    <Typography classes={{ root: 'subtitle-dialog-1' }}>WO Algorithm</Typography>
+                    </div>
+                    {/* <List>
+                        <ListItem>
+                            <Typography classes={{ root: 'title-dialog-1' }}>Description {itemPressed == "" ? "" : "of " + itemPressed}</Typography>
+                            <ul id="item-align">
+                                <li id="list-item">
+                                    <Typography classes={{ root: 'subtitle-dialog-1' }}>
+                                        This recommendation is used to suggest the optimal time to wash your solar panels.
+                                    </Typography>
+                                </li>
+                            </ul>
+                            <p>Yo</p>
+                            <p>Test</p>
+                        </ListItem>
+                        <Divider classes={{ root: 'divider-item' }} />
+                        <ListItem>
+                            <Typography classes={{ root: 'title-dialog-1' }}>Inputs</Typography>
+                            <ol id="list-align">
+                                <Typography classes={{ root: 'subtitle-dialog-1' }}>
+                                    <li>Input 1</li>
+                                    <li>Input 2</li>
+                                    <li>Input 3</li>
+                                </Typography>
+                            </ol>
+                        </ListItem>
+                        <Divider classes={{ root: 'divider-item' }} />
+                        <ListItem>
+                            <Typography classes={{ root: 'title-dialog-1' }}>Assets</Typography>
+                            <ul id="item-align">
+                                <li id="list-item">
+                                    <Typography classes={{ root: 'subtitle-dialog-1' }}>
+                                        Solar Panels
+                                    </Typography>
+                                </li>
+                            </ul>
+                        </ListItem>
+                        <Divider classes={{ root: 'divider-item' }} />
+                        <ListItem>
+                            <Typography classes={{ root: 'title-dialog-1' }}>Algorithm</Typography>
+                            <ul id="item-align">
+                                <li id="list-item">
+                                    <Typography classes={{ root: 'subtitle-dialog-1' }}>
+                                        WO Algorithm
+                                        </Typography>
+                                </li>
+                            </ul>
+                        </ListItem>
+                    </List> */}
                 </div>
             </div>
 
