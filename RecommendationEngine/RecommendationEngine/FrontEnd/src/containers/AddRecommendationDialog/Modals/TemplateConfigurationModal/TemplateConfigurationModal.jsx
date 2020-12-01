018112909f2fca96ef1aeb6aff6e315f28e24360
CSB <cbahenduzi@gmail.com>
2020-12-01 06:55:39 -0500

    RE-5 - Selection of Template Buttons on Dialog 1

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AddRecommendationDialog/Modals/TemplateConfigurationModal/TemplateConfigurationModal.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AddRecommendationDialog/Modals/TemplateConfigurationModal/TemplateConfigurationModal.jsx
@@ -3,17 +3,17 @@ import { animated } from 'react-spring';
 import { Typography } from '@material-ui/core';
 import './TemplateConfigurationModal.css'
 import { TemplateItems } from './ListTemplateItems.ts';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 
 function TemplateConfigurationModal(props) {
 
-    const [itemPressed, setItemPressed] = useState("");
 
+    const [itemPressed, setItemPressed] = useState({ name: "", isPressed: true });
+    
     const TemplateCard = (props) => {
         return (
-            <div id="item-template" onClick={props.onClick}>
+             
+            <div id={props.isPressed == false ? "item-template-default": props.itemName!= props.name ? "item-template-default" : "item-template-selected"} onClick={props.onClick}>
                 <div>
                     <props.icon id="template-icon" />
                 </div>
@@ -29,88 +29,43 @@ function TemplateConfigurationModal(props) {
             <div id="parent-div">
                 <div id="template-grid">
                     {TemplateItems.map((item, index) => (
-                        <TemplateCard name={item.name}
-                            icon={item.listItemIcon}
-                            onClick={() => setItemPressed(item.name)}
-                        />))}
+
+                        <TemplateCard
+                        name={item.name}
+                        icon={item.listItemIcon}
+                        onClick={() => setItemPressed({ name: item.name, isPressed: (item.name == itemPressed.name? !itemPressed.isPressed : true) })}
+                        isPressed={itemPressed.isPressed}
+                        itemName={itemPressed.name}/>
+
+                    ))}
                 </div>
                 <div id="info-div">
-
-
-
-                    <div id="item-info1"> 
-                    <Typography classes={{ root: 'title-dialog-1' }}>Description {itemPressed == "" ? "" : "of " + itemPressed}</Typography>
-                    <Typography classes={{ root: 'subtitle-dialog-1' }}>This recommendation is used to suggest the optimal time to wash your solar panels.</Typography>
+                    <div id="item-info1">
+                        <Typography classes={{ root: 'title-dialog-1' }}>Description {itemPressed.name == "" ? "" : "of " + itemPressed.name}</Typography>
+                        <Typography classes={{ root: 'subtitle-dialog-1' }}>This recommendation is used to suggest the optimal time to wash your solar panels.</Typography>
                     </div>
                     <Divider classes={{ root: 'divider-item' }} />
                     <div id="item-info2">
-                    <Typography classes={{ root: 'title-dialog-1' }}>Inputs</Typography>
-                            <ol id="list-align">
-                                <Typography classes={{ root: 'list-dialog-1' }}>
-                                    <li>Input 1</li>
-                                    <li>Input 2</li>
-                                    <li>Input 3</li>
-                                </Typography>
-                            </ol>
-                            
+                        <Typography classes={{ root: 'title-dialog-1' }}>Inputs</Typography>
+                        <ol id="list-align">
+                            <Typography classes={{ root: 'list-dialog-1' }}>
+                                <li>Input 1</li>
+                                <li>Input 2</li>
+                                <li>Input 3</li>
+                            </Typography>
+                        </ol>
+
                     </div>
                     <Divider classes={{ root: 'divider-item' }} />
                     <div id="item-info3">
-                    <Typography classes={{ root: 'title-dialog-1' }}>Assets</Typography>
-                    <Typography classes={{ root: 'subtitle-dialog-1' }}>Solar Panels</Typography>
+                        <Typography classes={{ root: 'title-dialog-1' }}>Assets</Typography>
+                        <Typography classes={{ root: 'subtitle-dialog-1' }}>Solar Panels</Typography>
                     </div>
                     <Divider classes={{ root: 'divider-item' }} />
                     <div id="item-info4">
-                    <Typography classes={{ root: 'title-dialog-1' }}>Algorithm</Typography>
-                    <Typography classes={{ root: 'subtitle-dialog-1' }}>WO Algorithm</Typography>
+                        <Typography classes={{ root: 'title-dialog-1' }}>Algorithm</Typography>
+                        <Typography classes={{ root: 'subtitle-dialog-1' }}>WO Algorithm</Typography>
                     </div>
-                    {/* <List>
-                        <ListItem>
-                            <Typography classes={{ root: 'title-dialog-1' }}>Description {itemPressed == "" ? "" : "of " + itemPressed}</Typography>
-                            <ul id="item-align">
-                                <li id="list-item">
-                                    <Typography classes={{ root: 'subtitle-dialog-1' }}>
-                                        This recommendation is used to suggest the optimal time to wash your solar panels.
-                                    </Typography>
-                                </li>
-                            </ul>
-                            <p>Yo</p>
-                            <p>Test</p>
-                        </ListItem>
-                        <Divider classes={{ root: 'divider-item' }} />
-                        <ListItem>
-                            <Typography classes={{ root: 'title-dialog-1' }}>Inputs</Typography>
-                            <ol id="list-align">
-                                <Typography classes={{ root: 'subtitle-dialog-1' }}>
-                                    <li>Input 1</li>
-                                    <li>Input 2</li>
-                                    <li>Input 3</li>
-                                </Typography>
-                            </ol>
-                        </ListItem>
-                        <Divider classes={{ root: 'divider-item' }} />
-                        <ListItem>
-                            <Typography classes={{ root: 'title-dialog-1' }}>Assets</Typography>
-                            <ul id="item-align">
-                                <li id="list-item">
-                                    <Typography classes={{ root: 'subtitle-dialog-1' }}>
-                                        Solar Panels
-                                    </Typography>
-                                </li>
-                            </ul>
-                        </ListItem>
-                        <Divider classes={{ root: 'divider-item' }} />
-                        <ListItem>
-                            <Typography classes={{ root: 'title-dialog-1' }}>Algorithm</Typography>
-                            <ul id="item-align">
-                                <li id="list-item">
-                                    <Typography classes={{ root: 'subtitle-dialog-1' }}>
-                                        WO Algorithm
-                                        </Typography>
-                                </li>
-                            </ul>
-                        </ListItem>
-                    </List> */}
                 </div>
             </div>
 
