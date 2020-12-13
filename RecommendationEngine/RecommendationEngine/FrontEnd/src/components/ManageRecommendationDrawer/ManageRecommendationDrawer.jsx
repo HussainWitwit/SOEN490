bd6d4346a3fcb6f1d7600ccc559ae521736bd104
DariushN <35940376+DariushN@users.noreply.github.com>
2020-12-12 22:19:24 -0500

    RE-103 - Clean up code and add responsive recurrence display

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/ManageRecommendationDrawer/ManageRecommendationDrawer.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/ManageRecommendationDrawer/ManageRecommendationDrawer.jsx
@@ -1,153 +1,208 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import './ManageRecommendationDrawer.css';
 import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import 'date-fns';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
 import Tooltip from '@material-ui/core/Tooltip';
 import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
-// import {getConfiguredRecommendationById} from '../../api/get/ConfiguredRecommendationEndpoints'
-
-
-ManageRecommendationDrawer.propType = {
-  isInternalClosed: PropTypes.bool.isRequired,
-  isDrawerPinned: PropTypes.bool.isRequired
-}
-
 
 //Extracting props instead of calling props everytime. Might be less readable. However, dev experience is amazing. A.J.U.U
-export default function ManageRecommendationDrawer({ isInternalClosed, isDrawerPinned, configuredRecommendation }) {
-
-  const [isPinClicked, setIsPinClicked] = useState(false);
-
+export default function ManageRecommendationDrawer({
+  configuredRecommendation,
+}) {
   // Animation style
   const props = useSpring({
     opacity: 1,
     transform: 'translate3d(0px,0,0)',
     from: { opacity: 0, transform: 'translate3d(20px,0,0)' },
-  })
+  });
 
+  var formatYear = { month: 'long', day: 'numeric' };
+  var formatMonth = { day: 'numeric' };
+  var formatTime = { hour: 'numeric', minute: '2-digit', hour12: true };
+  var dayOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-  // const [data, setData] = useState([]);
-
-  // const fetchData = async () => {
-  //   let response = await getConfiguredRecommendationById(selectedScheduleId);
-  //   setData(response);
-  // }
-
-  const pinDrawerEvent = () => {
-    setIsPinClicked(!isPinClicked);
-    isDrawerPinned(!isPinClicked)
-  }
-
-  const editRecommendation = () => {
-    console.log("Edit a recommendation")
-  }
+  const stringRecurrenceFormatting = (granularity, dateInput, repeatDay) => {
+    let date = new Date(dateInput);
+    switch (granularity) {
+      case 'Yearlyy':
+        return `Every year on ${
+          date.toLocaleTimeString('en-us', formatYear).split(',')[0]
+        } at ${date.toLocaleTimeString('en-us', formatTime)}`;
+      case 'Yearlyyy':
+        return `Every ${
+          date.toLocaleTimeString('en-us', formatMonth).split(',')[0]
+        }th of the month at ${date.toLocaleTimeString('en-us', formatTime)}`;
+      case 'Yearly':
+        return `Every ${dayOfWeek[repeatDay - 1]} at ${date.toLocaleTimeString(
+          'en-us',
+          formatTime
+        )}`;
+      default:
+        return 'Invalid';
+    }
+  };
 
   return (
     <animated.div style={props}>
-      <div className='drawer-content'>
+      <div className="drawer-content">
         <Grid container>
           <Grid item xs={12}>
-            <p className='drawer-title'>{configuredRecommendation.name}</p>
-            <p className='drawer-subtitle'>Description</p>
-            <div className='drawer-description'>{configuredRecommendation.description}</div>
-          </Grid>
-          <Grid item xs={12}>
-            <div className='recommendationType'>
-              <p className='value-title'>Type</p>
-              <p className='values'>{configuredRecommendation.type}</p>
+            <p className="drawer-title">{configuredRecommendation.name}</p>
+            <p className="drawer-subtitle">Description</p>
+            <div className="drawer-description">
+              {configuredRecommendation.description}
             </div>
           </Grid>
           <Grid item xs={12}>
-            <div className='assets'>
-              <p className='value-title'>Assets</p>
-              {configuredRecommendation.assetList && configuredRecommendation.assetList.map((asset, index) => {
-                if (configuredRecommendation.assetList.length == index + 1) {
-                  return <div className='asset-values'>{asset.displayText} </div>
-                }
-                else {
-                  return <div className='asset-values'>{asset.displayText}, </div>
-                }
-              })}
+            <div className="recommendationType">
+              <p className="value-title">Type</p>
+              <p className="values">{configuredRecommendation.type}</p>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div className="assets">
+              <p className="value-title">Assets</p>
+              {configuredRecommendation.assetList &&
+                configuredRecommendation.assetList.map((asset, index) => {
+                  if (configuredRecommendation.assetList.length == index + 1) {
+                    return (
+                      <div className="asset-values">{asset.displayText} </div>
+                    );
+                  } else {
+                    return (
+                      <div className="asset-values">{asset.displayText}, </div>
+                    );
+                  }
+                })}
             </div>
           </Grid>
           <Grid item xs={8}>
-            <div className='inputs'>
-              <p className='value-title'>Parameters</p>
-              {configuredRecommendation.parameters && configuredRecommendation.parameters.map((parameter) => {
-                return <div className='values'>{parameter.parameterName}</div>
-              })}
+            <div className="inputs">
+              <p className="value-title">Parameters</p>
+              {configuredRecommendation.parameters &&
+                configuredRecommendation.parameters.map((parameter) => {
+                  return (
+                    <div className="values">{parameter.parameterName}</div>
+                  );
+                })}
             </div>
           </Grid>
           <Grid item xs={4}>
-            <div className='outputs'>
-              <p className='value-title'>Value</p>
-              {configuredRecommendation.parameters && configuredRecommendation.parameters.map((parameter) => {
-                return <div className='values'>{parameter.parameterValue}</div>
-              })}
+            <div className="outputs">
+              <p className="value-title">Value</p>
+              {configuredRecommendation.parameters &&
+                configuredRecommendation.parameters.map((parameter) => {
+                  return (
+                    <div className="values">{parameter.parameterValue}</div>
+                  );
+                })}
             </div>
           </Grid>
           <Grid item xs={12}>
-            <div className='assets'>
-              <p className='value-title'>Preferred Scenario</p>
-              <p className='values'>{configuredRecommendation.preferredScenario}</p>
+            <div className="assets">
+              <p className="value-title">Preferred Scenario</p>
+              <p className="values">
+                {configuredRecommendation.preferredScenario}
+              </p>
             </div>
           </Grid>
           <Grid item xs={12}>
-            <div className='assets'>
-              <p className='value-title'>Schedule</p>
-              <p className='values'>{configuredRecommendation.granularity}</p>
+            <div className="assets">
+              <p className="value-title">Schedule</p>
+              <p className="values">
+                {configuredRecommendation.granularity &&
+                  configuredRecommendation.recurrenceDayOfWeek &&
+                  configuredRecommendation.recurrenceDatetime &&
+                  stringRecurrenceFormatting(configuredRecommendation.granularity, configuredRecommendation.recurrenceDatetime, configuredRecommendation.recurrenceDayOfWeek)}
+              </p>
             </div>
           </Grid>
           <Grid item xs={12}>
-            <p className='value-title'>Last Five Executions</p>
-            <div className='last-five-status'>
-              {configuredRecommendation.lastJobs && configuredRecommendation.lastJobs.map((value) => {
-                return value == null ?
-                  <Tooltip title="No status available"><div className="Empty"></div></Tooltip>
-                  : <Tooltip title={<span><p>Id: {value.id}</p><p>Status: {value.status}</p><p>Date: {value.timestamp}</p></span>}><div className={value.status}></div></Tooltip>
-              })}
+            <p className="value-title">Last Five Executions</p>
+            <div className="last-five-status">
+              {configuredRecommendation.lastJobs &&
+                configuredRecommendation.lastJobs.map((value) => {
+                  return value == null ? (
+                    <Tooltip title="No status available">
+                      <div className="Empty"></div>
+                    </Tooltip>
+                  ) : (
+                    <Tooltip
+                      title={
+                        <span>
+                          <p>Id: {value.id}</p>
+                          <p>Status: {value.status}</p>
+                          <p>Date: {value.timestamp}</p>
+                        </span>
+                      }
+                    >
+                      <div className={value.status}></div>
+                    </Tooltip>
+                  );
+                })}
             </div>
           </Grid>
           <Grid item xs={12}>
-            <p className='value-title'>Last Execution Status</p>
-                {configuredRecommendation.lastJobs && configuredRecommendation.lastJobs[4] ?
-              <div className={'execution-status-' + (configuredRecommendation.lastJobs && configuredRecommendation.lastJobs[4] && configuredRecommendation.lastJobs[4].status)}>
-                <Chip label={configuredRecommendation.lastJobs && configuredRecommendation.lastJobs[4] && configuredRecommendation.lastJobs[4].status.toUpperCase()} />
+            <p className="value-title">Last Execution Status</p>
+            {configuredRecommendation.lastJobs &&
+            configuredRecommendation.lastJobs[4] ? (
+              <div
+                className={
+                  'execution-status-' +
+                  (configuredRecommendation.lastJobs &&
+                    configuredRecommendation.lastJobs[4] &&
+                    configuredRecommendation.lastJobs[4].status)
+                }
+              >
+                <Chip
+                  label={
+                    configuredRecommendation.lastJobs &&
+                    configuredRecommendation.lastJobs[4] &&
+                    configuredRecommendation.lastJobs[4].status.toUpperCase()
+                  }
+                />
               </div>
-                :
+            ) : (
               <div className={'execution-status-Empty'}>
                 <Chip label="NO STATUS" />
               </div>
-            }
+            )}
           </Grid>
           <Grid item xs={12}>
-            <div className='created-edited-by'>
-              <p className='edited-by'>Last edited by: </p>
-              <p className='created-edited-by-name'>{configuredRecommendation.createdBy}</p>
+            <div className="created-edited-by">
+              <p className="edited-by">Last edited by: </p>
+              <p className="created-edited-by-name">
+                {configuredRecommendation.createdBy}
+              </p>
             </div>
           </Grid>
           <Grid item xs={12}>
-            <div className='edit-recommendation-button'>
+            <div className="edit-recommendation-button">
               <Button variant="outlined">Edit a Recommendation</Button>
             </div>
           </Grid>
           <Grid item xs={12}>
-            <div className='force-run-button'>
+            <div className="force-run-button">
               <Button variant="outlined">Force run</Button>
             </div>
           </Grid>
           <Grid item xs={12}>
-            <div className='delete-button'>
+            <div className="delete-button">
               <Button variant="outlined">Delete</Button>
             </div>
           </Grid>
         </Grid>
       </div>
-    </animated.div >
+    </animated.div>
   );
 }
