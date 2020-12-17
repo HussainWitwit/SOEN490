1af54f2d9b2c7d71f9fceab937ed02edc9ceb162
CSB <cbahenduzi@gmail.com>
2020-12-16 22:47:19 -0500

    RE-6 - Setup FE with mocked JSON

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/TemplateConfigurationModal/ListTemplateItems.ts
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/TemplateConfigurationModal/ListTemplateItems.ts
@@ -10,36 +10,61 @@ import { MdPermDeviceInformation } from 'react-icons/md';
 type ListTemplateObject = {
     name: string,
     listItemIcon: OverridableComponent<SvgIconTypeMap>,
+    description: string,
+    inputList: string [],
+    algorithName: string
 };
 
 var washOptimization: ListTemplateObject = {
     name: 'Yearly Wash Optimization',
     listItemIcon: FaSolarPanel,
+    description: "This recommendation is used to suggest the optimal time to wash your solar panels. The algorithm takes in consideration:  dates  of soiling seasons, the rate of soiling, the energy price, predicated energy, cost of cleaning and more.",
+    inputList: ["Center Point", "Span Increment", "Soiling Season Buffer", "Acccelerator"],
+    algorithName:"Yearly Wash Optimization Algorithm"
 };
 
 var gearboxReplacement: ListTemplateObject = {
     name: 'Gear Replacement',
     listItemIcon: BsFillGearFill,
+    description: "Description of GR Algorithm",
+    inputList: ["Input 1", "Input 2", "Input 3", "Input 4"],
+    algorithName:"GR Algorithm"
+
 };
 
 var panelAngle: ListTemplateObject = {
     name: 'Panel Angle',
     listItemIcon: BiShapeTriangle,
+    description: "Description of PA Algorithm",
+    inputList: ["Input 5", "Input 6", "Input 7", "Input 8"],
+    algorithName:"PA Algorithm"
+
 };
 
 var fuseReplacement: ListTemplateObject = {
     name: 'Fuse Replacement',
     listItemIcon: GiBatteryPackAlt ,
+    description: "Description of F Algorithm",
+    inputList: ["Input 9", "Input 10", "Input 11", "Input 12"],
+    algorithName:"F Algorithm"
+
 };
 
 var etc: ListTemplateObject = {
     name: 'ETC',
     listItemIcon: VscCircuitBoard,
+    description: "Description of ETC Algorithm",
+    inputList: ["Input 13", "Input 14", "Input 15", "Input 16"],
+    algorithName:"GR Algorithm"
+
 };
 
 var other: ListTemplateObject = {
     name: 'Other',
     listItemIcon: MdPermDeviceInformation,
+    description: "Description of Other Algorithm",
+    inputList: ["Input 17", "Input 18", "Input 19", "Input 20"],
+    algorithName:"GR Algorithm"
 };
 
 export const TemplateItems = [washOptimization, gearboxReplacement, fuseReplacement, panelAngle, etc, other]
--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/TemplateConfigurationModal/TemplateConfigurationModal.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/TemplateConfigurationModal/TemplateConfigurationModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { animated } from 'react-spring';
 import { Typography } from '@material-ui/core';
 import { TemplateItems } from './ListTemplateItems.ts';
@@ -7,7 +7,11 @@ import './TemplateConfigurationModal.css';
 import { connect } from 'react-redux';
 import { mapDialogStateToProps, mapDispatchToProps, mapDispatchMergedToProps } from '../../redux/AddRecDialogReducer/reducer-actions';
 
-function TemplateConfigurationModal ({ template, dialogStyle, setTemplateName, templateDetailsList }) {
+function TemplateConfigurationModal({ template, dialogStyle, setTemplateName, templateDetailsList }) {
+
+  const [templateDescription, setTemplateDescription] = useState("This recommendation is used to suggest the optimal time to wash your solar panels. The algorithm takes in consideration:  dates  of soiling seasons, the rate of soiling, the energy price, predicated energy, cost of cleaning and more.");
+  const [inputList, setInputList] = useState(["Center Point", "Span Increment", "Soiling Season Buffer", "Acccelerator"]);
+  const [algorithmName, setAlgorithmName] = useState("Yearly Wash Optimization Algorithm");
 
   const TemplateCard = (props) => {
     return (
@@ -36,43 +40,56 @@ function TemplateConfigurationModal ({ template, dialogStyle, setTemplateName, t
               icon={item.listItemIcon}
               onClick={() => {
                 setTemplateName(TemplateItems[index].name);
+                setTemplateDescription(TemplateItems[index].description);
+                setAlgorithmName(TemplateItems[index].algorithName);
+                setInputList(TemplateItems[index].inputList);
+
               }}
               isPressed={item.name === template.name}
             />
           ))}
         </div>
+
+
         <div id="info-div">
           <div id="item-info1">
-            <Typography classes={{ root: 'title-dialog-1' }}>
-              Description {template.name}
+            <Typography classes={{ root: 'title-dialog-0' }}>
+            {template.name}
             </Typography>
-            <Typography classes={{ root: 'subtitle-dialog-1' }}>
-              This recommendation is used to suggest the optimal time to wash
-              your solar panels.
-            </Typography>
+
+            {templateDescription.split(".").map((item, index) => (
+
+              <Typography classes={{ root: 'subtitle-dialog-1' }}>
+                {item}
+              </Typography>
+
+            ))}
+
           </div>
           <Divider classes={{ root: 'divider-item' }} />
           <div id="item-info2">
-            <Typography classes={{ root: 'title-dialog-1' }}>Inputs</Typography>
+            <Typography classes={{ root: 'title-dialog-1' }}>Parameters Inputs</Typography>
             <ol id="list-align">
               <Typography classes={{ root: 'list-dialog-1' }}>
-                <li>Input 1</li>
-                <li>Input 2</li>
-                <li>Input 3</li>
+                {inputList.map((item) => (
+                  <li>{item}</li>
+                ))}
+
               </Typography>
             </ol>
           </div>
 
           <Divider classes={{ root: 'divider-item' }} />
-          <div id="item-info4">
+          <div id="item-info3">
             <Typography classes={{ root: 'title-dialog-1' }}>
               Algorithm
             </Typography>
             <Typography classes={{ root: 'subtitle-dialog-1' }}>
-              WO Algorithm
+              {algorithmName}
             </Typography>
           </div>
         </div>
+
       </div>
     </animated.div>
   );
