d1cba3279967d0a7672e7a6d7dafc88374e3a06a
DariushN <35940376+DariushN@users.noreply.github.com>
2020-12-24 18:41:19 -0500

    RE-244 - Remove unnecessary coupling from new merge files

--- RecommendationEngine/RecommendationEngine/Services/RecommendationTypeService.cs
+++ RecommendationEngine/RecommendationEngine/Services/RecommendationTypeService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Interfaces.Services;
-using Interfaces.Services.ExternalAPI;
 using Models.Recommendation;
 using Interfaces.Repositories;
 using System.Linq;
@@ -12,12 +11,10 @@ namespace RecommendationEngine.Services
     public class RecommendationTypeService : IRecommendationTypeService
     {
 
-        private IDriveService _driveService;
         private IRecommendationTypeRepository _recommendationTypeRepository;
 
-        public RecommendationTypeService(IDriveService driveService, IRecommendationTypeRepository recommendationTypeRepository)
+        public RecommendationTypeService(IRecommendationTypeRepository recommendationTypeRepository)
         {
-            _driveService = driveService;
             _recommendationTypeRepository = recommendationTypeRepository;
         }
 
