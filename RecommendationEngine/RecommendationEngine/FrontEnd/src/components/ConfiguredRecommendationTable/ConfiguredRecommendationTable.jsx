64e167c6f7c898c0f4a751b257ff885095a43112
CSB <cbahenduzi@gmail.com>
2021-01-17 21:38:38 -0500

    RE-115 - Add Sorting to Configured Recommendation Table

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/ConfiguredRecommendationTable/ConfiguredRecommendationTable.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/ConfiguredRecommendationTable/ConfiguredRecommendationTable.jsx
@@ -1,46 +1,88 @@
-import React from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Toolbar from '@material-ui/core/Toolbar';
-import Paper from '@material-ui/core/Paper';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { EnhancedTableHead } from '../../components/RecommendationTableHeader/RecommendationTableHeader'
-import Switch from '@material-ui/core/Switch';
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Toolbar from "@material-ui/core/Toolbar";
+import Paper from "@material-ui/core/Paper";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import { EnhancedTableHead } from '../../components/RecommendationTableHeader/RecommendationTableHeader';
 import { mapDispatchToProps } from '../../redux/RightPanelReducer/reducer-actions'
 import { connect } from 'react-redux';
 import './ConfiguredRecommendationTable.css';
 import 'date-fns';
 
 const headCells = [
-  { id: 'title', label: 'Title' },
-  { id: 'type', label: 'Type' },
-  { id: 'granularity', label: 'Granularity' },
-  { id: 'createdOn', label: 'Created On' },
+  { id: "name", numeric: false, disablePadding: true, label: "Title"},
+  { id: "type", numeric: false, disablePadding: false, label: "Type" },
+  { id: "granularity", numeric: false, disablePadding: false, label: "Granularity" },
+  { id: "createdOn", numeric: false, disablePadding: false, label: "Created On" },
 ];
 
-//TODO: This should be a generic component, ask Alain if help is needed.
+function createData(title, type, granularity, createdOn) {
+  return { title, type, granularity, createdOn };
+}
+
+const rows = [
+  createData("Cupcake", "Yearly Wash Optimization", "Yearly", "2021-01-16"),
+  createData("Donut", 452, 25.0, 51),
+  createData("Eclair", 262, 16.0, 24),
+  createData("Frozen yoghurt", 159, 6.0, 24),
+  createData("Gingerbread", 356, 16.0, 49),
+  createData("Honeycomb", 408, 3.2, 87),
+  createData("Ice cream sandwich", 237, 9.0, 37),
+  createData("Jelly Bean", 375, 0.0, 94),
+  createData("KitKat", 518, 26.0, 65),
+  createData("Lollipop", 392, 0.2, 98),
+  createData("Marshmallow", 318, 0, 81),
+  createData("Nougat", 360, 19.0, 9),
+  createData("Oreo", 437, 18.0, 63)
+];
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
 export function ConfiguredRecommendationTable(props) {
-
-  const [dense, setDense] = React.useState(false);
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('');
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("createdOn");
   const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [isSelected, setIsSelected] = React.useState(null);
-
-  //TODO: Beware for data as props
 
   const handleChangeDense = (event) => {
     setDense(event.target.checked);
   };
 
-  const handleRequestSort = (property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
 
@@ -66,20 +108,30 @@ export function ConfiguredRecommendationTable(props) {
             label={<h6 id="controlLabel">Lite</h6>}
           />
         </Toolbar>
+
         <TableContainer>
           <Table
-            id="table" aria-labelledby="tableTitle" size={dense ? 'small' : 'medium'} aria-label="enhanced table"
+            id="table"
+            aria-labelledby="tableTitle"
+            size={dense ? "small" : "medium"}
+            aria-label="enhanced table"
           >
-            <EnhancedTableHead id="handleSort" order={order} orderBy={orderBy}
+            <EnhancedTableHead
+              id="handleSort"
+              order={order}
+              orderBy={orderBy}
+              headCells={headCells}
               onRequestSort={handleRequestSort}
-              headers={headCells}
-              rowCount={props.data ? props.data.length : 1}
             />
-            <TableBody id="table-body" data-testid="table-body-cypress">
-              {props.data && props.data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((element) => {
-                return (
-                  <TableRow
+            <TableBody>
+              {stableSort(props.data, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((element, index) => {
+
+                  return (
+                    <TableRow
                     hover
+                    tabIndex={-1}
                     selected={isSelected === element.id}
                     key={element.id}
                     className="custom"
@@ -88,22 +140,24 @@ export function ConfiguredRecommendationTable(props) {
                       setIsSelected(element.id)
                     }}
                   >
-                    <TableCell className="custom">
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                      padding="default"
-                      className="primaryKey"
-                      id="tableBody">
-                      {element.name}
-                    </TableCell>
-                    <TableCell className="custom" id="tableBody">{element.type}</TableCell>
-                    <TableCell className="custom" id="tableBody">{element.granularity}</TableCell>
-                    <TableCell className="custom" id="tableBody">{element.createdOn}</TableCell>
-                  </TableRow>
-                )
-              })}
+                      <TableCell padding="checkbox">
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        padding="none"
+                        className="primaryKey"
+                        id="tableBody"
+                      >
+                        {element.name}
+                      </TableCell>
+                      <TableCell align="custom" id="tableBody">{element.type}</TableCell>
+                      <TableCell align="custom" id="tableBody">{element.granularity}</TableCell>
+                      <TableCell align="custom" id="tableBody">{element.createdOn}</TableCell>
+                    </TableRow>
+                  );
+                })}
+
             </TableBody>
           </Table>
         </TableContainer>
@@ -111,6 +165,8 @@ export function ConfiguredRecommendationTable(props) {
           id="pagination"
           component="div"
           rowsPerPageOptions={[10, 25, 50, 100]}
+          component="div"
+          count={rows.length}
           count={props.data ? props.data.length : 1}
           rowsPerPage={rowsPerPage}
           page={page}
--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/RecommendationTableHeader/RecommendationTableHeader.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/RecommendationTableHeader/RecommendationTableHeader.jsx
@@ -5,51 +5,47 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import './RecommendationTableHeader.css';
-
-
-//TODO: To review 
-// -> stateless component
 export function EnhancedTableHead(props) {
-    const { order, orderBy, onRequestSort } = props;
-    const createSortHandler = (property) => (event) => {
-      onRequestSort(event, property);
-    };
-  
-    return (
-      <TableHead id="table-head" className="custom">
-        <TableRow id="table-row">
-          <TableCell id="table-cell" className="custom"></TableCell>
-          {props.headers.map((headCell) => (
-            <TableCell
-              className="custom"
-              key={headCell.id}
-              // align={headCell.numeric ? 'left' : 'center'}
-              sortDirection={orderBy === headCell.id ? order : false}
-              id="tableHeader"
+
+  const { order, orderBy, headCells, onRequestSort } = props;
+
+  const createSortHandler = (property) => (event) => { onRequestSort(event, property) };
+
+  return (
+    <TableHead id="table-head" className="custom">
+      <TableRow id="table-row">
+        <TableCell></TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            id="tableHeader"  
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "default"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={true}
+              classes={{ active: 'sortLabel' }}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
             >
-              <TableSortLabel
-                id="sort-label"
-                className="custom"
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={createSortHandler(headCell.id)}
-              >
-                {headCell.label}
-                {orderBy === headCell.id ? (
-                  <span id="visuallyHidden">
-                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                  </span>
-                ) : null}
-              </TableSortLabel>
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-    );
-  }
-  
-  EnhancedTableHead.propTypes = {
-    onRequestSort: PropTypes.func,
-    order: PropTypes.oneOf(['asc', 'desc']),
-    orderBy: PropTypes.string,
-  };
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span id="visuallyHidden">
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+   EnhancedTableHead.propTypes = {
+   classes: PropTypes.object.isRequired,
+   headCell: PropTypes.object.isRequired,
+   onRequestSort: PropTypes.func.isRequired,
+   order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+   orderBy: PropTypes.string.isRequired,
+ };
