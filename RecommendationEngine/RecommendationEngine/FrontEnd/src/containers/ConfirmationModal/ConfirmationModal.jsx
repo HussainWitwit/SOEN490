665a61869bfc0613674e7310f11556b406f6086f
Badreddine Loulidi <badred123456@gmail.com>
2021-01-25 23:19:03 -0500

    RE-317 - Remove unused comment

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ConfirmationModal/ConfirmationModal.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ConfirmationModal/ConfirmationModal.jsx
@@ -17,21 +17,6 @@ export function ConfirmationModal (props) {
 
   const { dialogStyle, dialogsContent } = props;
 
-  // const stringRecurrenceFormatting = (dialogsContent.basicConfiguration.granularity, dialogsContent.basicConfiguration.repeatDate, dialogsContent.basicConfiguration.repeatDay) => {
-  //   let date = dialogsContent.basicConfiguration.repeatDate;
-  //   switch (dialogsContent.basicConfiguration.granularity) {
-  //     case 'Yearly':
-  //       return (`Every year on ${date.toLocaleTimeString('en-us', formatYear).split(',')[0]} at ${date.toLocaleTimeString('en-us', formatTime)}`);
-  //     case 'Monthly':
-  //       return (`Every ${date.toLocaleTimeString('en-us', formatMonth).split(',')[0]}th of the month at ${date.toLocaleTimeString('en-us', formatTime)}`);
-  //     case 'Weekly':
-  //       let intDayOfWeek = dialogsContent.basicConfiguration.repeatDay;
-  //       return (`Every ${dayOfWeek[intDayOfWeek - 1]} at ${date.toLocaleTimeString('en-us', formatTime)}`);
-  //     default:
-  //       return "Invalid";
-  //   }
-  // }
-
   return (
     <animated.div id="confirmation-modal-container" style={dialogStyle}>
       <div id="confirmation-modal-content">
