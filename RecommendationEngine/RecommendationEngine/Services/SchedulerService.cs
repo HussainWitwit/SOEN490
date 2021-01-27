7d6130965ed1926b3e9681b350e2add176f1c015
DariushN <35940376+DariushN@users.noreply.github.com>
2021-01-26 19:35:33 -0500

    RE-10 - Implement controller and service for scheduler

---
+++ RecommendationEngine/RecommendationEngine/Services/SchedulerService.cs
@@ -0,0 +1,18 @@
+﻿using Interfaces.RecommendationScheduler;
+using Interfaces.Services;
+
+namespace RecommendationEngine.Services
+{
+    public class SchedulerService: ISchedulerService
+    {
+        private IRecommendationScheduler _scheduler;
+        public SchedulerService(IRecommendationScheduler scheduler)
+        {
+            _scheduler = scheduler;
+        }
+        public void ScheduleJob(int id)
+        {
+            _scheduler.TriggerJobAsync(id);
+        }
+    }
+}
