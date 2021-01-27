fb512298a74091241d66e2406a027bda9af9faa9
DariushN <35940376+DariushN@users.noreply.github.com>
2021-01-27 00:34:31 -0500

    RE-10 - Add exception handling for BE

--- RecommendationEngine/RecommendationEngine/Services/SchedulerService.cs
+++ RecommendationEngine/RecommendationEngine/Services/SchedulerService.cs
@@ -1,5 +1,8 @@
-﻿using Interfaces.RecommendationScheduler;
+﻿using System;
+using Interfaces.RecommendationScheduler;
 using Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using RecommendationEngine.ExceptionHandler;
 
 namespace RecommendationEngine.Services
 {
@@ -12,7 +15,14 @@ namespace RecommendationEngine.Services
         }
         public void ScheduleJob(int id)
         {
-            _scheduler.TriggerJobAsync(id);
+            try
+            {
+                _scheduler.TriggerJobAsync(id);
+            }
+            catch (Exception e)
+            {
+                throw new GlobalException(StatusCodes.Status500InternalServerError, "Internal Server Error", e.Message, "Recommendation Engine");
+            }
         }
     }
 }
