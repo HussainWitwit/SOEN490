29ec79ff0dc12d4d4b24b60d302a5284b7a68837
Badreddine Loulidi <badred123456@gmail.com>
2021-01-28 15:53:31 -0500

    RE-10 reverting

---
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/ForceRunPopUp/ForceRunPopUp.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import CloseIcon from '@material-ui/icons/Close';
+import { IconButton } from '@material-ui/core';
+import { mapDispatchPopUpActions } from '../../redux/RightPanelReducer/reducer-actions';
+import { connect } from 'react-redux';
+import {ForceRunConfiguredRecommendation} from '../../api/endpoints/ConfiguredRecommendationEndpoints'
+import "./ForceRunPopUp.css"
+
+export function ForceRunPopUp(props) {
+
+    const handleClose = () => {
+        props.handleForceRunPopUpOpen();
+    };
+
+    return (
+        <Dialog
+            classes={{ paper: "dialog" }}
+            open={props.open}
+            onClose={handleClose}
+        >
+            <IconButton aria-label="close" id="closeButton2" onClick={handleClose}>
+                <CloseIcon />
+            </IconButton>
+            <div id="deleteWarning2">
+                <div id="warningMessage2">
+                    <DialogTitle classes={{ root: 'alertMessage' }}><b>Force Run Configured Recommendation</b></DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Are you sure you want to force run <b>{props.title}</b>?
+                </DialogContentText>
+                    </DialogContent>
+                </div>
+            </div>
+            <DialogActions>
+                <div id="buttons2">
+                    <Button onClick={handleClose} id="cancelButton2" variant="outlined">
+                        Cancel
+          </Button>
+                    <Button onClick={() => {
+                        handleClose();
+                        props.updateScheduleDrilldown('forceRun');
+                        ForceRunConfiguredRecommendation(props.recommendationId);
+                    }} id="deleteButton2" variant="outlined">
+                        Force Run
+          </Button>
+                </div>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default connect(null, mapDispatchPopUpActions)(ForceRunPopUp)
