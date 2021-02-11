43fe8180da0ffcdb45639c026e279635d67fb1fb
CSB <cbahenduzi@gmail.com>
2021-02-11 18:44:08 -0500

    RE-170 - Mock Jobs Table

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
@@ -30,9 +30,9 @@ function Dashboard() {
         <div>
             <div>
                 Dashboard!
-           </div>
+          </div>
 
-            <div style={{ height: 400, width: '100%' }}>
+            {/* <div style={{ height: 400, width: '100%' }}>
                 <DataGrid
                     columns={[
                         {field: 'id', headerName: 'Result ID', width: 150, cellClassName: 'table-style'},
@@ -57,8 +57,63 @@ function Dashboard() {
                         Toolbar: GridToolbar, 
                     }}
                 />
+            </div> */}
+
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    columns={[
+                        {field: 'id', headerName: 'Job ID', width: 150, cellClassName: 'table-style'},
+
+                        {field: 'status',
+                         headerName: 'Status', 
+                         type: 'string',
+                         flex: 0.20,
+                         headerAlign: 'center',
+                         renderCell: (params) => (
+                           <div 
+                           className={
+                               params.getValue('status') === "Running"? 'job-status-running' :
+                               params.getValue('status') === "Failed"? 'job-status-failed' : 'job-status-success'}
+                            >
+                               {params.getValue('status')}
+                           </div>
+                         )},
+
+                        {field: 'timestamp', headerName: 'Timestamp', type: 'dateTime', flex: 0.20, cellClassName: 'table-style'},
+                        {field: 'duration', headerName: 'Job Duration', flex: 0.20, cellClassName: 'table-style'},
+                        {field: 'configuredRecommendationTitle', headerName: 'Configured Recommendation', type: 'string', flex: 0.20, cellClassName: 'table-style'},
+                        {field: 'jobLog',
+                         headerName: 'Job Log',
+                         flex: 0.20,
+                         headerAlign: 'center',
+                         renderCell: (params) => (
+                        <JobLogPopUp 
+                        className={"job-log-style"}
+                        jobId={params.getValue('id')}
+                        >
+                        </JobLogPopUp>
+                         )}
+                    ]}
+
+                    rows={[
+                        {id: 176, status: 'Failed', timestamp: '2021-01-15T12:15:25', duration: '2'+" seconds", configuredRecommendationTitle: 'algoDemo'},
+                        {id: 177, status: 'Success', timestamp: '2021-01-15T12:23:26', duration: '10'+" seconds", configuredRecommendationTitle: 'finalTestYWO'},
+                        {id: 178, status: 'Running', timestamp: '2021-01-15T12:23:39', duration: '17'+" seconds", configuredRecommendationTitle: 'finaTest'}
+                    ]}
+
+                    // onCellClick={}
+                    // onCellHover={}
+                    // onRowClick={}
+                    // onRowSelected={}
+                    // filterModel={riceFilterModel}
+                    showToolbar
+                    components={{
+                        Toolbar: GridToolbar, 
+                    }}
+                />
             </div>
 
+           
 
         </div>
 
