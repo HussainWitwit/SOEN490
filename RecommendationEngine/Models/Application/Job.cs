c83999b4154a5ea87eb4336486cd2ff86b5d4f11
Zohal <zohalmir9@gmail.com>
2021-02-12 22:22:01 -0500

    return rec. id of jobs and action Grouping

--- RecommendationEngine/Models/Application/Job.cs
+++ RecommendationEngine/Models/Application/Job.cs
@@ -6,7 +6,8 @@ namespace Models.Application
     {
         public int Id { get; set; }
         public int Duration { get; set; }
-        public string configuredRecommendationTitle { get; set; } 
+        public int ConfiguredRecommendationId { get; set; }
+        public string ConfiguredRecommendationTitle { get; set; } 
         public string Status { get; set; }
         public DateTime Timestamp { get; set; }
     }
