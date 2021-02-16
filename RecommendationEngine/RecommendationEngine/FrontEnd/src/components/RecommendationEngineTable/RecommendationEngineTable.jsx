94a04fe55f8abf691d4716a3239bb0eb1fcb359b
CSB <cbahenduzi@gmail.com>
2021-02-16 03:59:47 -0500

    RE-170 - Test fix

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/RecommendationEngineTable/RecommendationEngineTable.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/RecommendationEngineTable/RecommendationEngineTable.jsx
@@ -11,6 +11,7 @@ export default function RecommendationEngineTable(props) {
     <div id="root">
       <div style={{height: 800, width: 1230}}>
         <DataGrid
+          test-id='data-grid'
           component="div"
           pageSize={25}
           density={'compact'}
