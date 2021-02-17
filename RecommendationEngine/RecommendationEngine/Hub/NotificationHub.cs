2c675603a8be9ddb40a4a44326b81d8606f60e43
DariushN <35940376+DariushN@users.noreply.github.com>
2021-02-17 00:14:23 -0500

    RE-355 - Add signalr hub

---
+++ RecommendationEngine/RecommendationEngine/Hub/NotificationHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Interfaces.Hub;
+using Microsoft.AspNetCore.SignalR;
+
+namespace RecommendationEngine.Hub
+{
+    public class NotificationHub: Microsoft.AspNetCore.SignalR.Hub, INotificationHub
+    {
+        public async Task SendNotification(string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+    }
+}
