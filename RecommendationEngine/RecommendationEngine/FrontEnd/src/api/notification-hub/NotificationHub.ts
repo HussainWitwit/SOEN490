2e3c094df7f83f167ef8661c7efd7829196e5d8a
DariushN <35940376+DariushN@users.noreply.github.com>
2021-02-17 18:04:11 -0500

    RE-355 - Add client notification hub singleton

---
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/api/notification-hub/NotificationHub.ts
@@ -0,0 +1,29 @@
+import * as signalR from "@microsoft/signalr";
+
+class NotificationHub {
+  private static instance: NotificationHub;
+
+  private constructor() {
+    const hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("/notificationhub")
+    .build();
+ 
+    // Starts the SignalR connection
+    hubConnection.start();
+   }
+
+  public static getInstance(): NotificationHub {
+      if (!NotificationHub.instance) {
+        NotificationHub.instance = new NotificationHub();
+      }
+
+      return NotificationHub.instance;
+  }
+
+}
+
+// useEffect(() => {
+//   hubConnection.on("setTime", message => {
+//     setTime(message);
+//   });
+// });
