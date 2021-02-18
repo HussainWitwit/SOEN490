b06c95e3226e00898e24ed5fc249a2c1ebc52515
Kenza <kenza.boulisfane@gmail.com>
2021-02-18 11:51:15 -0500

    RE-58 - Create function to rename asset keys

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/utilities/ArrayManipulationUtilities.ts
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/utilities/ArrayManipulationUtilities.ts
@@ -1,5 +1,6 @@
 import { Parameter } from "../api/models/TemplateDetails";
 import { dateFormat } from './DateTimeUtilities';
+import { Asset } from '../api/models/Asset';
 export const convertObjectToArrayOfObjects = (obj: any) => {
     var result = Object.entries(obj).map((e) => ({ [e[0]]: e[1] }));
     return result;
@@ -22,4 +23,18 @@ export const transformParameterListPost = (array: Parameter[]): any[] => {
             parameterValue: value?.toString()
         };
     })
+}
+
+export const renameKeys = (array: Asset[]): any[] => {
+    let newArray: Asset[] = array;
+    let resultArray: any[];
+    resultArray = newArray.map(element => (
+        {
+            key: element.id,
+            title: element.displayText,
+            children: element.children,
+            value: element.id
+        }
+    ))
+    return resultArray;
 }
