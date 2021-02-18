c692ffd40046b40ddb7550698979392217fa2def
DariushN <35940376+DariushN@users.noreply.github.com>
2021-02-17 20:08:17 -0500

    RE-355 - Complete NotificationAlert connection with signalR

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/components/NotificationAlert/NotificationAlert.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/components/NotificationAlert/NotificationAlert.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import NotificationHub from '../../api/notification-hub/NotificationHub';
+import 'react-toastify/dist/ReactToastify.css';
 import './NotificationAlert.css';
 
 export default function NotificationAlert() {
   const notify = () => toast('Wow so easy!');
-  const notificationHub = NotificationHub.getInstance();
-  const [notificationMessage, setNotificationMesage] = React.useState('');
-  React.useEffect(() => {
-    hubConnection.on('ReceiveNotification', (message) => {
-      toast.info(message, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        });
+  const notificationHub = NotificationHub.getHubConnection();
+  const handleNotification = (message) => toast.info(message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
     });
-  });
+  React.useEffect(() => {
+    notificationHub.on('ReceiveNotification', (message) => {
+      handleNotification(message);
+    });
+  }, [notificationHub.on('ReceiveNotification')]);
   return (
     <div>
-      <button onClick={notify}>Notify!</button>
       <ToastContainer />
     </div>
   );
