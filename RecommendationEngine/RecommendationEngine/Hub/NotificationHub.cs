d66a3ca068a90b9616279055d26887b6853fab23
DariushN <35940376+DariushN@users.noreply.github.com>
2021-02-18 03:14:16 -0500

    RE-355 - Send notification to single test user

--- RecommendationEngine/RecommendationEngine/Hub/NotificationHub.cs
+++ RecommendationEngine/RecommendationEngine/Hub/NotificationHub.cs
@@ -5,18 +5,20 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace RecommendationEngine.Hub
 {
+    // This authorize tag might throw an exception in the UI if no access token is sent
     [Authorize]
     public class NotificationHub: Microsoft.AspNetCore.SignalR.Hub, INotificationHub
     {
         private IHubContext<NotificationHub> _context;
         public NotificationHub(IHubContext<NotificationHub> context)
         {
-            this._context = context;
+            _context = context;
         }
 
         public async Task SendNotification(string message)
         {
-            await _context.Clients.All.SendAsync("ReceiveNotification", message);
+            // This should eventually be changed to send notifications to specific groups once defined with proper authentication.
+            await _context.Clients.User("Koooohoooookoooooo").SendAsync("ReceiveNotification", message);
         }
     }
 }
