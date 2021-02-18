40c2e1442c087ef84284f07eda107cfc4433aa3f
DariushN <35940376+DariushN@users.noreply.github.com>
2021-02-17 20:19:25 -0500

    RE-355 - Add authorization to hub

--- RecommendationEngine/RecommendationEngine/Hub/NotificationHub.cs
+++ RecommendationEngine/RecommendationEngine/Hub/NotificationHub.cs
@@ -1,14 +1,22 @@
 ﻿using System.Threading.Tasks;
 using Interfaces.Hub;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace RecommendationEngine.Hub
 {
+    [Authorize]
     public class NotificationHub: Microsoft.AspNetCore.SignalR.Hub, INotificationHub
     {
+        private IHubContext<NotificationHub> _context;
+        public NotificationHub(IHubContext<NotificationHub> context)
+        {
+            this._context = context;
+        }
+
         public async Task SendNotification(string message)
         {
-            await Clients.All.SendAsync("ReceiveNotification", message);
+            await _context.Clients.All.SendAsync("ReceiveNotification", message);
         }
     }
 }
