c8ad8786c4a8053a7d400a3a196cad565c5d01cb
Zohal <zohalmir9@gmail.com>
2021-02-19 01:35:05 -0500

    migrations to add asset type to recommendation type

---
+++ RecommendationEngine/Models/DB/DBAssetTypeRecommendationType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models.DB
+{
+    [Table("AssetTypeRecommendationType")]
+    public class DBAssetTypeRecommendationType
+    {
+        public int AssetTypeId { get; set; }
+        public int RecommendationTypeId { get; set; }
+        public DBAssetType AssetType { get; set; }
+        public DBRecommendationType RecommendationType { get; set; }
+    }
+}
---
+++ RecommendationEngine/Models/Migrations/20210219041756_AddAssetTypeToRecType.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Models.Migrations
+{
+    public partial class AddAssetTypeToRecType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AssetTypeRecommendationType",
+                columns: table => new
+                {
+                    AssetTypeId = table.Column<int>(nullable: false),
+                    RecommendationTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssetTypeRecommendationType", x => new { x.AssetTypeId, x.RecommendationTypeId });
+                    table.ForeignKey(
+                        name: "FK_AssetTypeRecommendationType_AssetType_AssetTypeId",
+                        column: x => x.AssetTypeId,
+                        principalTable: "AssetType",
+                        principalColumn: "AssetTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AssetTypeRecommendationType_RecommendationType_Recommendatio~",
+                        column: x => x.RecommendationTypeId,
+                        principalTable: "RecommendationType",
+                        principalColumn: "RecommendationTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetTypeRecommendationType_RecommendationTypeId",
+                table: "AssetTypeRecommendationType",
+                column: "RecommendationTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssetTypeRecommendationType");
+        }
+    }
+}
