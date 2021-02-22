d159e08da0270e16b85375b56e7a272444bdc6ed
Badreddine Loulidi <badred123456@gmail.com>
2021-02-22 17:04:33 -0500

    RE-368 - Added styling for wash dates

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import { Calendar } from "react-modern-calendar-datepicker";
+import './Dashboard.css';
 
 function Dashboard() {
     const [selectedDay, setSelectedDay] = useState(null);
-
+    
     return (
         <div>
             <Calendar
                 value={selectedDay}
                 onChange={setSelectedDay}
-                shouldHighlightWeekends
                 colorPrimary="#4dd3ef"
-            />
+                customDaysClassName={[
+                    { year: 2021, month: 2, day: 23, className: 'washDay' },
+                ]}
+                shouldHighlightWeekends
+                />
         </div>
     )
 }
