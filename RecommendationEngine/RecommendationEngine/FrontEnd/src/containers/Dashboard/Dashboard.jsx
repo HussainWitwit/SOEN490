98ddd3f6adc9001493366c33a6004000d9ad4d8c
Badreddine Loulidi <badred123456@gmail.com>
2021-02-22 17:19:43 -0500

    RE-368 - Add the page header "Dashboad" with the subtitle

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import { Calendar } from "react-modern-calendar-datepicker";
+import { Grid } from '@material-ui/core';
 import './Dashboard.css';
 
 function Dashboard() {
     const [selectedDay, setSelectedDay] = useState(null);
-    
+
     return (
         <div>
+            <div>
+                <br></br>
+                <Grid id="grid-container1" container spacing={1} className="gridContainerStyle">
+                    <Grid id="grid1" item>
+                        <h3 id="title">Dashboard</h3>
+                        <h6 id="subtitle">View a calendar with upcomming wash days</h6>
+                    </Grid>
+                </Grid>
+                <br></br>
+            </div>
             <Calendar
                 value={selectedDay}
                 onChange={setSelectedDay}
