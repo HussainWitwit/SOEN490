47ca90160dc9a4c4f1df85b634d6268b0779b43d
Kenza <kenza.boulisfane@gmail.com>
2021-02-26 21:25:43 -0500

    RE-369 - set up the frontend for the Dashboard
    Co-Authored-By: Loujain Al-Nakeeb <44000696+Loujain-Al-Nakeeb@users.noreply.github.com>

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/Dashboard/Dashboard.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import './Dashboard.css';
+import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined'; import Tooltip from '@material-ui/core/Tooltip';
 
-function Dashboard() {
+function Dashboard () {
 
-    return (
-            <div>
-                Dashboard!
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div id='widget-container'>
+        <div id='net-savings-widget'>
+          <div id='tooltip-container'>
+            <Tooltip title="info">
+              <HelpOutlineOutlinedIcon size={20} />
+            </Tooltip>
           </div>
-    )
+          <div id='title-container'>Potential Net Savings</div>
+          <div>
+            <div id='widget-contents'>
+              <div id='sign'>$</div>
+              <div id='money-value'>638734</div>
+            </div>
+          </div>
+        </div>
+        <div id='roi-widget'>
+          <div id='tooltip-container'>
+            <Tooltip title="info">
+              <HelpOutlineOutlinedIcon size={20} />
+            </Tooltip>
+          </div>
+          <div id='title-container'>Potential ROI</div>
+          <div>
+            <div id='widget-contents'>
+              <div id='sign'>%</div>
+              <div id='money-value'>157</div>
+            </div>
+          </div>
+        </div>
+        <div id='inaction-widget'><div id='tooltip-container'>
+          <Tooltip title="info">
+            <HelpOutlineOutlinedIcon size={20} />
+          </Tooltip>
+        </div>
+          <div id='title-container'>Potential Losses</div>
+          <div>
+            <div id='widget-contents'>
+              <div id='sign'>$</div>
+              <div id='money-value'>5682</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard;
