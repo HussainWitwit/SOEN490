a53f87f148e727e94b76dd9242febe8e8dcd0d29
Badreddine Loulidi <badred123456@gmail.com>
2021-03-03 23:47:26 -0500

    RE-368 - Fixed the link in the results table and switched asset column to second

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ResultsPage/ResultsPage.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ResultsPage/ResultsPage.jsx
@@ -39,17 +39,17 @@ export function ResultsPage(props) {
     const columns = [
         { field: 'id', headerName: 'Result ID', width: 150, cellClassName: 'table-style', hide: true },
         { field: 'resultOutputDate', headerName: 'Timestamp', type: 'string', flex: 0.14, cellClassName: 'table-style' },
+        { field: 'assetName', headerName: 'Asset', type: 'string', flex: 0.14, cellClassName: 'table-style' },
         { field: 'netSaving', headerName: 'Net Saving', type: 'number', ...CADPrice, flex: 0.14, cellClassName: 'table-positive-numbers' },
         { field: 'returnOnInvestment', headerName: 'Return On Investment', type: 'number', ...PercentageOption, flex: 0.14, cellClassName: 'table-positive-numbers' },
         { field: 'costOfAction', headerName: 'Cost of Action', type: 'number', ...CADPrice, flex: 0.14, cellClassName: 'table-negative-numbers' },
         { field: 'costOfInaction', headerName: 'Cost of Inaction', type: 'number', ...CADPrice, flex: 0.14, cellClassName: 'table-negative-numbers' },
         {
             field: 'configuredRecommendationTitle', headerName: 'Recommendation', type: 'string', flex: 0.14, cellClassName: 'table-style', renderCell: (params) => (
-                <a className='configured-recommendation' nClick={() => openScheduleDrilldown(params.getValue('configuredRecommendationId'))}>
+                <a className='configured-recommendation' onClick={() => openScheduleDrilldown(params.getValue('configuredRecommendationId'))}>
                     {params.getValue('configuredRecommendationTitle')}
                 </a>)
-        },
-        { field: 'assetName', headerName: 'Asset', type: 'string', flex: 0.14, cellClassName: 'table-style' }
+        }
     ]
 
     const getResultList = async () => {
