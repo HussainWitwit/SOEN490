b8b84e2dc3a20596304c1ec2b5e65bc51195de19
CSB <cbahenduzi@gmail.com>
2021-03-23 17:07:21 -0400

    Code clean up and datagrid update

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ResultsPage/ResultsPage.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ResultsPage/ResultsPage.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Grid } from '@material-ui/core';
 import RecommendationEngineTable from '../../components/RecommendationEngineTable/RecommendationEngineTable';
-import SearchBar from '../../common/SearchBar';
 import { GetRecommendationResultList } from '../../api/endpoints/ResultsEndpoints';
 import { mapDispatchToProps } from '../../redux/RightPanelReducer/reducer-actions';
 import { mapStateToProps as mapAssetFilterStateToProps } from '../../redux/AssetFilterReducer/reducer-actions';
