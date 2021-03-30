3aa451407347b4f37819cd197ee58ad487d93d54
CSB <cbahenduzi@gmail.com>
2021-03-30 13:20:48 -0400

    Descending order sorting for the Manage Page table

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ManageRecommendationPage/ManageRecommendationPage.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ManageRecommendationPage/ManageRecommendationPage.jsx
@@ -49,7 +49,7 @@ export function ManageRecommendationPage (props) {
         onClickRow={openScheduleDrilldown}
         isClickable={true}
         dateColumnName={'createdOn'}
-        dateSortingOrder={'asc'}
+        dateSortingOrder={'desc'}
 
       />
     </div >
--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ManageRecommendationPage/TableConfig.tsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/ManageRecommendationPage/TableConfig.tsx
@@ -1,5 +1,5 @@
 export const TableColumns = [
-    {field: 'id', headerName: 'ID', width: 150, cellClassName: 'table-style', hide: false},
+    {field: 'id', headerName: 'ID', width: 150, cellClassName: 'table-style', hide: true},
     {field: 'name', headerName: 'Title', flex:0.25, type: 'string', cellClassName: 'table-style'},
     {field: 'type', headerName: 'Type', flex: 0.25, type: 'string', cellClassName: 'table-style'},
     {field: 'granularity', headerName: 'Granularity', type: 'string', flex: 0.25, cellClassName: 'table-style'},
