ca85e5871cba64cb23f8f2226bb3637bf463be82
Badreddine Loulidi <badred123456@gmail.com>
2021-04-03 21:37:06 -0400

    Fix sonarqube code smells

--- RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AssetTreeView/AssetTreeView.jsx
+++ RecommendationEngine/RecommendationEngine/FrontEnd/src/containers/AssetTreeView/AssetTreeView.jsx
@@ -55,9 +55,8 @@ function AssetIcon({ type, energyType }) {
           return <FontAwesomeIcon className='label-icon' icon={faSun} />;
         case 'WIND':
           return <FontAwesomeIcon className='label-icon' icon={faWind} />;
-        default:
-          break;
       }
+      return;
     default:
       return <FontAwesomeIcon className='label-icon' icon={faCubes} />;
   }
